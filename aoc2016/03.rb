c0c6cc850350168cf5cacdd67b0c1c08428562ba
Daniel Rødskog <danielr@mesan.no>
2016-12-03 15:09:06 +0100

    Solve AOC2016 day 3 part 2 in Ruby

--- aoc2016/03.rb
+++ aoc2016/03.rb
@@ -1,3 +1,12 @@
+check_triangle = ->(tuple) { a,b,c = tuple.sort; a + b > c }
+
 input = File.readlines('03_input.txt').map { |line| line.split.map(&:to_i) }
 
-p input.count { |tuple| a,b,c = tuple.sort; a + b > c }
+# Part 1
+p input.count(&check_triangle)
+
+# Part 2
+p input
+  .each_slice(3)
+  .map { |group| group.transpose.count(&check_triangle) }
+  .reduce(:+)
