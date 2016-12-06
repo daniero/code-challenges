16dae8d5fbee0389d97cac90a0219e6610b9d582
Daniel Rødskog <danielr@mesan.no>
2016-12-06 08:33:28 +0100

    Solve AOC2016 day 6 part 2

--- aoc2016/06.rb
+++ aoc2016/06.rb
@@ -2,4 +2,8 @@ input = File.open('06_input.txt').each_line.map { |line| line.chop.chars }
 
 cols = input.transpose.map { |col| col.sort_by { |char| col.count(char) } }
 
+# Part 1
 p cols.transpose.last.join
+
+# Part 2
+p cols.transpose.first.join
