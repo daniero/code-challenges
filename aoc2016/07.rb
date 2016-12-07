414d5ce1f99e0fc0d4f50f702a0b9bc3b6b1c564
Daniel Roedskog <danielmero@gmail.com>
2016-12-07 17:27:59 +0100

    Solve AOC2016 day 7 part 2

--- aoc2016/07.rb
+++ aoc2016/07.rb
@@ -3,13 +3,30 @@ class String
     self =~ /(.)(.)\2\1/ && $1 != $2
   end
 
-  def tls?
+  def bracket_split
     split = self.split(/\[|\]|\n/)
-    outside_brackets, inside_brackets = split.partition.with_index { |_, i| i.even? }
+    split.partition.with_index { |_, i| i.even? }
+  end
+
+  def tls?
+    outside_brackets, inside_brackets = bracket_split
 
     outside_brackets.any?(&:abba?) && inside_brackets.none?(&:abba?)
   end
+
+  def ssl?
+    outside_brackets, inside_brackets = bracket_split
+
+    outside_brackets.any? { |string|
+      string.chars.each_cons(3).any? { |a,b,c|
+        a == c && a != b && inside_brackets.grep(/#{b}#{a}#{b}/).any?
+      }
+    }
+  end
 end
 
-
+# Part 1
 puts File.open('07_input.txt').each_line.count(&:tls?)
+
+# Part 2
+puts File.open('07_input.txt').each_line.count(&:ssl?)
