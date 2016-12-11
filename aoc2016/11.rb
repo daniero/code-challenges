67a24a33be83380d0d329c4629b028abb72d278f
Daniel Rødskog <danielr@mesan.no>
2016-12-11 21:48:38 +0100

    Implement breadth-first search
    Incorrect solution since it assumes two microchips cans be brought to
    the same floor at the same time when the floor contains both
    corresponding generators

--- aoc2016/11.rb
+++ aoc2016/11.rb
@@ -1,21 +1,34 @@
-Configuration = Struct.new(:moves, :elevator, :floors)
+Configuration = Struct.new(:moves, :elevator, :floors, :prev) do
+  def eql?(other)
+    sort_floor = ->(floor) { floor.sort_by { |c| [c.isotope, c.class.name] } }
+
+    elevator == other.elevator &&
+      floors.zip(other.floors).all? { |a,b| sort_floor[a] == sort_floor[b] }
+  end
+
+  def hash
+    elevator.hash ^ floors.hash
+  end
+end
 
 Generator = Struct.new(:isotope)
 Microchip = Struct.new(:isotope) do
-  def safe?(generators)
-    generators.empty? || generators.any? { |g| g.isotope == isotope }
+  def safe?(generators, microchips)
+    generators.empty? ||
+      generators.any? { |g| g.isotope == isotope } ||
+      generators.all? { |g| microchips.any? { |c| c.isotope == g.isotope } }
   end
 end
 
 def done?(configuration)
-  configuration[0..2].all? { |floor| floor.empty? }
+  configuration.floors[0..2].all? { |floor| floor.empty? }
 end
 
 def ok?(configuration)
   configuration.floors.all? do |floor|
     generators, microchips = floor.partition { |component| component.is_a? Generator }
 
-    microchips.all? { |chip| chip.safe?(generators) }
+    microchips.all? { |chip| chip.safe?(generators, microchips) }
   end
 end
 
@@ -28,14 +41,16 @@ def transistions(configuration)
   # Floors to try
   [+1, -1].each do |direction|
     to_index = elevator + direction
-    next unless to_index >= 0 && to_index <= 4
+    next if to_index < 0 || to_index >= 4
 
     to_floor = floors[to_index]
 
     # Things to move
     (1..2).each do |amount|
+      next if amount > from_floor.size
       [*0...from_floor.size].combination(amount) do |take_indexes|
         take_components = from_floor.values_at(*take_indexes)
+        take_components
 
         new_configuration = Configuration.new(moves + 1, to_index, floors.dup)
         new_configuration.floors[elevator] = from_floor - take_components
@@ -52,11 +67,66 @@ def transistions(configuration)
   new_configurations
 end
 
-def read_start_configuration
-  floors = File.open('input/11.txt').map do |line|
+def read_start_configuration(filename)
+  floors = File.open(filename).map do |line|
     line
       .scan(/(\w+) generator|(\w+)-compatible microchip/)
-      .map { |gen, chip| gen ? Generator.new(gen.to_sym) : Microchip.new(chip.to_sym) }
+      .map { |gen, chip| (gen ? Generator.new(gen.to_sym) : Microchip.new(chip.to_sym)) }
   end
   Configuration.new(0, 0, floors)
 end
+
+require 'pp'
+require 'set'
+
+def search(*configurations)
+  visited = Set.new
+  trail = {}
+  id = 0
+
+  loop do
+    configuration = configurations.shift
+
+    puts
+    pp configuration
+
+    return configuration, trail if done?(configuration)
+
+    trail[id] = configuration
+
+    reachable = transistions(configuration)
+    reachable.each do |new|
+      next if visited.include? new
+
+      p :NEW
+      pp new
+      new.prev = id
+      visited << new
+      configurations << new
+    end
+
+    id += 1
+  end
+end
+
+conf, trail = search(read_start_configuration('input/11_testcase.txt'))
+
+puts
+puts
+
+pp conf
+
+puts
+puts
+
+n = conf
+path = []
+loop do
+  path.unshift n
+  n = trail[n.prev]
+
+  break unless n
+end
+
+pp path
+
