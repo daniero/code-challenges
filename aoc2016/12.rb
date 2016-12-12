69263dc64174d2108e6fff86a9575e3d32149ea6
Daniel Rødskog <danielr@mesan.no>
2016-12-12 07:45:47 +0100

    Solve AOC2016 day 12 part 2

--- aoc2016/12.rb
+++ aoc2016/12.rb
@@ -2,7 +2,6 @@ reg = {"a"=>0, "b"=>0, "c"=>0, "d"=>0}
 ip = 0
 
 cmds = File.open('input/12.txt').map do |line|
-  puts line
   case line
   when /cpy ([a-z]) (\w)/
     val, dst = $1, $2
@@ -22,14 +21,26 @@ cmds = File.open('input/12.txt').map do |line|
   when /jnz (\d+) (-?\d+)/
     val, dst = $1.to_i, $2.to_i
     ->() {ip += (val == 0 ? 1 : dst)}
-  else
-    puts "huh?"
   end
 end
 
-loop do
-  p [ip, reg]
-  break unless ip < cmds.size
 
+# Part 1
+
+while ip < cmds.size
   cmds[ip%cmds.size].call()
 end
+
+p reg
+
+# Part 2
+
+reg = {"a"=>0, "b"=>0, "c"=>1, "d"=>0}
+ip = 0
+
+while ip < cmds.size
+  cmds[ip%cmds.size].call()
+end
+
+
+p reg
