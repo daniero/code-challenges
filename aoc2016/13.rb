4bbdf153a315b73925719ff7ab94caf0691fd6ae
Daniel Rødskog <danielr@mesan.no>
2016-12-13 08:49:06 +0100

    Solve AOC2016 day 13 part 1

---
+++ aoc2016/13.rb
@@ -0,0 +1,47 @@
+require 'set'
+require 'pqueue'
+
+INPUT = 1350
+TARGET = [31,39]
+
+DIRECTIONS = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
+def distance(x,y)
+  i,j = TARGET
+  Math.sqrt((x-i)**2 + (y-j)**2)
+end
+
+walls = Hash.new do |h, (x, y)|
+  n = x*x + 3*x + 2*x*y + y + y*y + INPUT
+  bits = (0...n.bit_length).map { |i| n[i] }
+  h[[x,y]] = bits.count(1).odd?
+end
+
+# moves, [x,y]
+start = [0, [1,1]]
+queue = PQueue.new([start]) { |a, b| distance(*a[1]) < distance(*b[1]) }
+
+
+def search(walls, queue)
+  visited = Set.new
+
+  loop do
+    moves, (x,y) = queue.pop
+    return moves if [x,y] == TARGET
+
+    visited << [x,y]
+
+    DIRECTIONS.each do |i, j|
+      new_x, new_y = x + i, y + j
+      next if visited.include?([new_x, new_y])
+      next if walls[[new_x, new_y]]
+      next if new_x < 0 && new_y < 0
+
+      try = [moves + 1, [new_x, new_y]]
+      queue.push(try)
+    end
+
+  end
+end
+
+puts search(walls, queue)
