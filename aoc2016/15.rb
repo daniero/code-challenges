ce81e02d0e0732a890dadbe248212666ccc2edb1
Daniel Roedskog <danielmero@gmail.com>
2016-12-15 12:30:03 +0100

    Solve AOC2016 day 15 part 2 in Ruby

--- aoc2016/15.rb
+++ aoc2016/15.rb
@@ -5,11 +5,18 @@ input = File.open(FILE).each_line.map do |line|
   positions, start = [$1, $2].map(&:to_i)
 end
 
-x = 1.step.find { |time|
-  input.each_with_index.all? { |(positions, start), i|
-    current = (start + time + i) % positions
-    current == 0
+def find_time(disks)
+  x = 1.step.find { |time|
+    disks.each_with_index.all? { |(positions, start), i|
+      current = (start + time + i) % positions
+      current == 0
+    }
   }
-}
+  x - 1
+end
 
-p x - 1
+# Part 1
+p find_time(input)
+
+# Part 2
+p find_time(input + [[11, 0]])
