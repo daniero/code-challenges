99a31ad3d9a3be8eecf5abd3504b9a56602f6fae
Daniel Rødskog <danielr@mesan.no>
2016-12-17 16:36:07 +0100

    Make search return an enumerator

--- aoc2016/17.rb
+++ aoc2016/17.rb
@@ -9,28 +9,33 @@ DIRECTIONS = {"U" => [0, -1], "D" => [0, 1], "L" => [-1, 0], "R" => [1, 0]}
 def search(start)
   queue = [[0, '', START]] # moves, path, coordinates
 
-  loop do
-    state = queue.shift
-    return nil unless state
+  Enumerator.new do |yielder|
+    loop do
+      state = queue.shift
+      break unless state
 
-    moves, path, (x,y) = state
-    return state if [x,y] == TARGET
+      moves, path, (x,y) = state
 
-    doors_open =
-      Digest::MD5.hexdigest(CODE+path)
-      .chars.take(4)
-      .map { |c| c =~ /[b-f]/ }
+      if [x,y] == TARGET
+        yielder << state
+        next
+      end
 
-    directions = DIRECTIONS.select.with_index { |_, i| doors_open[i] }
+      doors_open =
+        Digest::MD5.hexdigest(CODE+path)
+        .chars.take(4)
+        .map { |c| c =~ /[b-f]/ }
 
-    directions.each do |direction, (i, j)|
-      new_x, new_y = x + i, y + j
-      next if new_x < 0 || new_x > 3 || new_y < 0 || new_y > 3
+      directions = DIRECTIONS.select.with_index { |_, i| doors_open[i] }
 
-      queue << [moves + 1, path + direction, [new_x, new_y]]
+      directions.each do |direction, (i, j)|
+        new_x, new_y = x + i, y + j
+        next if new_x < 0 || new_x > 3 || new_y < 0 || new_y > 3
+
+        queue << [moves + 1, path + direction, [new_x, new_y]]
+      end
     end
   end
 end
 
-p search(START)
-
+p search(START).peek
