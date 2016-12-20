10c6014825142cff97b651ec7190a69d34dc98c1
Daniel Rødskog <danielr@mesan.no>
2016-12-20 20:19:01 +0100

    Keep blacklisted ip ranges as Range objects

--- aoc2016/20.rb
+++ aoc2016/20.rb
@@ -1,28 +1,28 @@
-def overlap?(start1, stop1, start2, stop2)
-  start1 <= start2 && stop1 >= start2 - 1 ||
-    start2 <= start1 && stop2 >= start1 - 1
+def overlap?(range1, range2)
+  min, max = [range1, range2].minmax_by(&:first)
+  min.cover?(max.min) || min.last + 1 == max.first
 end
 
-def merge_overlapping_ranges(ranges)
-  all_to, all_from = ranges.transpose
-  [all_to.min, all_from.max]
+def merge_overlapping_ranges(*ranges)
+  starts, stops = ranges.map { |range| [range.first, range.last] }.transpose
+  Range.new(starts.min, stops.max)
 end
 
 def merge_ranges(ranges)
-  ranges.reduce([]) do |previous, (current_start, current_stop)|
-    overlapping, distinct = previous.partition { |to, from| overlap?(to,from, current_start,current_stop) }
-
-    distinct + [merge_overlapping_ranges(overlapping << [current_start, current_stop])]
+  ranges.reduce([]) do |processed, current|
+    overlapping, distinct = processed.partition { |range| overlap?(range, current) }
+    merged = merge_overlapping_ranges(current, *overlapping)
+    [merged, *distinct]
   end
 end
 
 filename = ARGV[0] || 'input/20.txt'
-input = File.open(filename).each_line.map { |line| line.split('-').map(&:to_i) }
+input = File.open(filename).each_line.map { |line| Range.new(*line.split('-').map(&:to_i)) }
 
 blacklist = merge_ranges(input)
 
 # Part 1
-p blacklist.min.last + 1
+p blacklist.min_by(&:first).last + 1
 
 # Part 2
-p 2**32 - blacklist.reduce(0) { |count, (start, stop)| count + (start..stop).size }
+p 2**32 - blacklist.reduce(0) { |count, range| count + range.size }
