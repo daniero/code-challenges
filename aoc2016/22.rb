924011c29b4cbc20554ae37ee711cc2553c5cb66
Daniel Rødskog <danielr@mesan.no>
2016-12-22 19:41:55 +0100

    Keep x,y coordinates separate from node objects

--- aoc2016/22.rb
+++ aoc2016/22.rb
@@ -1,14 +1,14 @@
-Node = Struct.new(:x, :y, :size, :used, :avail, :use)
+Node = Struct.new(:size, :used, :avail, :use)
 
 input = File.open('input/22.txt').each_line.drop(2)
 
-nodes = input.map { |line|
+nodes = input.reduce({}) { |hash, line|
   line =~ /x(\d+)-y(\d+).*?(\d+)T.*?(\d+)T.*?(\d+)T.*?(\d+)%/
   _, x, y, size, used, avail, use = [*$~].map(&:to_i)
-  Node.new(x, y, size, used, avail, use)
+  hash.merge({ [x,y] => Node.new(size, used, avail, use) })
 }
 
-p nodes
+p nodes.values
   .repeated_permutation(2)
   .select { |a, b| a.used != 0 }
   .select { |a,b| a != b }
