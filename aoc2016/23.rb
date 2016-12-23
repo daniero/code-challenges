5d346eedf67d1dfcab561c4a0b0a554c3408cd3f
Daniel Rødskog <danielr@mesan.no>
2016-12-23 15:24:13 +0100

    Implement instruction toggle

--- aoc2016/23.rb
+++ aoc2016/23.rb
@@ -1,5 +1,5 @@
 def run(cmds, ip, reg)
-  case cmds[ip%cmds.size]
+  case p cmds[ip%cmds.size]
   when /cpy ([a-z]) (\w)/
     val, dst = $1, $2
     reg[dst] = reg[val]
@@ -23,16 +23,42 @@ def run(cmds, ip, reg)
   when /jnz (\d+) (-?\d+)/
     val, dst = $1.to_i, $2.to_i
     return ip + (val == 0 ? 1 : dst)
+
+  when /tgl ([a-z])/
+    dst = reg[$1] + ip
+    toggle(cmds, dst)
+
+  when /tgl (-?\d+)/
+    dst = $1.to_i + ip
+    toggle(cmds, dst)
   end
   ip + 1
 end
 
-cmds = File.read('input/12.txt').lines
+def toggle(cmds, dst)
+  if dst >= 0 && dst < cmds.size
+    subs = {"inc" => "dec",
+            "dec" => "inc",
+            "tgl" => "inc",
+            "jnz" => "cpy",
+            "cpy" => "jnz"}
+    from,to = subs.find { |cmd,_| cmds[dst].start_with? cmd }
+    cmds[dst].sub!(from, to)
+  end
+end
+
+cmds = File.read('input/23-test.txt').lines
 reg = {"a"=>0, "b"=>0, "c"=>0, "d"=>0}
 ip = 0
 
+require "pp"
+
 while ip < cmds.size
+  p ip
   ip = run(cmds, ip, reg)
+  pp reg
+  pp cmds
+  puts
 end
 
 puts reg["a"]
