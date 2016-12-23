dfd9edd48aeef6e9b06ff168ab40170fd3f213d7
Daniel Rødskog <danielr@mesan.no>
2016-12-23 14:45:48 +0100

    Read lines each time instead of mapping to lambdas

--- aoc2016/23.rb
+++ aoc2016/23.rb
@@ -1,31 +1,38 @@
+def run(cmds, ip, reg)
+  case cmds[ip%cmds.size]
+  when /cpy ([a-z]) (\w)/
+    val, dst = $1, $2
+    reg[dst] = reg[val]
+
+  when /cpy ([0-9]+) (\w)/
+    val, dst = $1.to_i, $2
+    reg[dst] = val
+
+  when /inc (\w)/
+    dst = $1
+    reg[dst] +=1
+
+  when /dec (\w)/
+    dst = $1
+    reg[dst] -=1
+
+  when /jnz ([a-z]) (-?\d+)/
+    val, dst = $1, $2.to_i
+    return ip + (reg[val] == 0 ? 1 : dst)
+
+  when /jnz (\d+) (-?\d+)/
+    val, dst = $1.to_i, $2.to_i
+    return ip + (val == 0 ? 1 : dst)
+  end
+  ip + 1
+end
+
+cmds = File.read('input/12.txt').lines
 reg = {"a"=>0, "b"=>0, "c"=>0, "d"=>0}
 ip = 0
 
-cmds = File.open('input/23.txt').map do |line|
-  case line
-  when /cpy ([a-z]) (\w)/
-    val, dst = $1, $2
-    ->() {reg[dst] = reg[val]; ip+=1}
-  when /cpy ([0-9]+) (\w)/
-    val, dst = $1.to_i, $2
-    ->() {reg[dst] = val; ip+=1}
-  when /inc (\w)/
-    dst = $1
-    ->() {reg[dst] +=1; ip+=1}
-  when /dec (\w)/
-    dst = $1
-    ->() {reg[dst] -=1; ip+=1}
-  when /jnz ([a-z]) (-?\d+)/
-    val, dst = $1, $2.to_i
-    ->() {ip += (reg[val] == 0 ? 1 : dst)}
-  when /jnz (\d+) (-?\d+)/
-    val, dst = $1.to_i, $2.to_i
-    ->() {ip += (val == 0 ? 1 : dst)}
-  end
-end
-
 while ip < cmds.size
-  cmds[ip%cmds.size].call()
+  ip = run(cmds, ip, reg)
 end
 
 puts reg["a"]
