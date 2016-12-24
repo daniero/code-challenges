64af2ce92b9fab99309a1f69041545c0b2b17938
Daniel Rødskog <danielr@mesan.no>
2016-12-24 01:55:25 +0100

    Solve AOC2016 day 19 part 2 in Ruby
    Full simulation, no fancy maths :D

--- aoc2016/19.rb
+++ aoc2016/19.rb
@@ -11,3 +11,47 @@ while elves.length > 1
 end
 
 puts elves.first
+
+# Part 2
+
+Elf = Struct.new(:num, :next_elf) do
+  def walk(n)
+    elf = self
+    n.times { elf = elf.next_elf }
+    elf
+  end
+
+  # Having presents stolen == leaving the circle == being dropped
+  def drop!
+    self.next_elf = self.next_elf.next_elf
+  end
+end
+
+def initialize_circle(n)
+  first = Elf.new(1)
+  last = first
+
+  2.upto(n) do |i|
+    new = Elf.new(i)
+    last.next_elf = new
+    last = new
+  end
+
+  last.next_elf = first
+end
+
+first = initialize_circle(INPUT)  # First elf to steal presents
+dropper = first.walk(INPUT/2 - 1) # Elf sitting before the first elf having their presents stolen
+
+n = INPUT
+i = 0
+
+(INPUT-1).times do
+  dropper.drop!
+  first = first.walk(1)
+  n -= 1
+  i += 1
+  dropper = dropper.walk(i % 2)
+end
+
+p first.num
