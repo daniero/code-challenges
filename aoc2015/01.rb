4eede6fc3215d528fc537385af5e66e7f202abf0
Daniel Rødskog <danielr@mesan.no>
2016-12-26 14:44:00 +0100

    Solve AOC2015 day 2 part 1 in Ruby

--- aoc2015/01.rb
+++ aoc2015/01.rb
@@ -1,1 +1,13 @@
-p File.read('input/01.txt').chomp.each_char.reduce(0) { |sum, char| sum + (char == '(' ? 1 : -1) }
+foo = File.read('input/01.txt')
+  .chomp
+  .each_char
+  .map { |char| char == '(' ? 1 : -1 }
+
+# Part 1
+p foo.reduce(:+)
+
+# Part 2
+p foo
+  .reduce([0]) { |a, f| a << a.last + f  }
+  .each_with_index.find { |f, i| f < 0 }
+  .last
