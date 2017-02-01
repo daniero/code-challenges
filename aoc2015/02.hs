fa42944dfad0c1237862a661ff85c292082682f9
Daniel Rødskog <danielr@mesan.no>
2017-02-01 21:12:43 +0100

    Solve AOC2015 day 2 pay 2 in Haskell

--- aoc2015/02.hs
+++ aoc2015/02.hs
@@ -5,8 +5,12 @@ area [l, w, h] = let a = l * w
                      c = h * l
                  in 2*a + 2*b + 2*c + minimum [a, b, c]
 
+ribbon dimensions = (sum dimensions - maximum dimensions) * 2 + product dimensions
+
 main = do
     input <- readFile "input/02.txt"
     let packages  = [ [ read x :: Int | x <- splitOn "x" p ] | p <- lines input ]
-    let dimensions = map area packages
-    print $ sum dimensions
+-- Part 1:
+    print $ sum $ map area packages
+-- Part 2:
+    print $ sum $ map ribbon packages
