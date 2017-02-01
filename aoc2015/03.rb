e3eba108b4b51d089012108143a240e9b5e1be70
Daniel Rødskog <danielr@mesan.no>
2017-02-01 21:54:17 +0100

    Solve AOC2015 day 3 in Ruby

---
+++ aoc2015/03.rb
@@ -0,0 +1,29 @@
+require 'set'
+
+def visited(directions)
+  x, y = 0, 0
+  visited = Set[[x, y]]
+
+  directions.each do |go_x, go_y|
+    x, y = x+go_x, y+go_y
+    visited << [x, y]
+  end
+
+  visited
+end
+
+directions = File.open('input/03.txt').each_char.map { |char|
+  case char
+  when '^' then [0, 1]
+  when '>' then [1, 0]
+  when 'v' then [0, -1]
+  when '<' then [-1, 0]
+  end
+}
+
+# Part 1
+puts visited(directions).size
+
+# Part 2
+santa, robot = directions.partition.with_index { |_,i| i.even? }
+puts (visited(santa) + visited(robot)).size
