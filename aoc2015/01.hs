fafbba8babef7ddceb694c2209112fa169aeb43a
Daniel Rødskog <danielr@mesan.no>
2017-02-01 17:53:46 +0100

    Solve AOC2015 day 1 part 2 in Haskell

--- aoc2015/01.hs
+++ aoc2015/01.hs
@@ -1,7 +1,15 @@
+import Data.List
+
 f '(' = 1
 f ')' = -1
 f _ = 0
 
 main = do
+-- Part 1:
     input <- readFile "input/01.txt"
-    print(sum(map f input))
+    let x = map f input
+    print(sum x)
+
+-- Part 2:
+    print(findIndex (<0) (scanl1 (+) (0:x)))
+
