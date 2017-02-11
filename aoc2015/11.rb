20956b749f9d17ea4a84d3d0135d6aed07b201c4
Daniel Rødskog <danielr@mesan.no>
2017-02-11 14:20:28 +0100

    Solve AOC2015 day 11 part 1 in Ruby

---
+++ aoc2015/11.rb
@@ -0,0 +1,25 @@
+password = "hxbxwxba"
+
+class String
+  def password_ok?
+    return false if self =~ /[iol]/
+    return false unless chars.each_cons(3).any? { |a,b,c| a.ord == b.ord-1 && b.ord == c.ord-1 }
+    return false unless self =~ /(.)\1.*(.)\2/
+    true
+  end
+
+  def next_password
+    s = dup
+    s.next! until s.password_ok?
+    s
+  end
+end
+
+# p "hijklmmn".password_ok?
+# p "abbceffg".password_ok?
+# p "abbcegjk".password_ok?
+# puts
+# p "abcdefgh".next_password # abcdffaa
+# p "ghijklmn".next_password # ghjaabcc
+
+puts password.next_password
