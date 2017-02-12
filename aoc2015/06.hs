8019f6b42b8628c0150fe8269049f8d0134030e0
Daniel Rødskog <danielr@mesan.no>
2017-02-12 19:10:42 +0100

    Solve AOC2015 day 6 part 1 in Haskell

--- aoc2015/06.hs
+++ aoc2015/06.hs
@@ -1,37 +1,30 @@
 import Data.List
+import Library
 
-allOff :: () -> (Int -> Int -> Bool)
-allOff () = (\x y -> False)
+start () = (\x y -> False)
 
 isIn x y x1 y1 x2 y2 = x1 <= x && x <= x2 && y1 <= y && y <= y2
 
-tOn :: Int -> Int -> Int -> Int -> (Int -> Int -> Bool) -> (Int -> Int -> Bool)
-tOn x1 y1 x2 y2 lights = (\x y -> isIn x y x1 y1 x2 y2 || lights x y)
+on x1 y1 x2 y2 lights = (\x y -> isIn x y x1 y1 x2 y2 || lights x y)
 
-tOff :: Int -> Int -> Int -> Int -> (Int -> Int -> Bool) -> (Int -> Int -> Bool)
-tOff x1 y1 x2 y2 lights = (\x y -> not (isIn x y x1 y1 x2 y2) && lights x y)
+off x1 y1 x2 y2 lights = (\x y -> not (isIn x y x1 y1 x2 y2) && lights x y)
 
-toggle :: Int -> Int -> Int -> Int -> (Int -> Int -> Bool) -> (Int -> Int -> Bool)
 toggle x1 y1 x2 y2 lights = (\x y -> if (isIn x y x1 y1 x2 y2) then not (lights x y) else (lights x y))
 
-n = 10
-start = [ [ allOff | _ <- [1..n] ] | _ <- [1..n] ]
+parseLine line = let [x1, y1, x2, y2] = readNumbers line
+                 in case line of
+                      _ | isPrefixOf "turn on" line -> on x1 y1 x2 y2
+                      _ | isPrefixOf "turn off" line -> off x1 y1 x2 y2
+                      _ | isPrefixOf "toggle" line -> toggle x1 y1 x2 y2
+
+n = 1000
 
 main = do
-  let x = allOff ()
-      y = tOn 1 1 3 3 x
-      z = tOn 5 5 5 5 y
-      a = tOff 1 1 3 3 z
-      b = toggle 0 0 4 4 a
-  print $ x 0 0
+  input <- readFile "input/06.txt"
+  let commands = lines input
+      init = start ()
+      lights = foldl (\fn line -> parseLine line fn) init commands
 
-  print $ y 0 0
-  print $ y 2 2
-  print $ z 4 4
-  print $ z 5 5
-  
-  print $ a 2 2
-  print $ a 5 5
+      coords = [(x,y) | x <- [0..n-1], y <- [0..n-1]]
 
-  print $ b 2 2
-  print $ b 5 5
+  print $ length $ filter (\(x,y) -> lights x y) coords
