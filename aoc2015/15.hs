289d2d3a5bc1bf733f1087f939c21c7e5f698cb6
Daniel Rødskog <danielr@mesan.no>
2017-02-12 01:05:52 +0100

    Solve AOC2015 day 15 part 1 in Haskell

--- aoc2015/15.hs
+++ aoc2015/15.hs
@@ -1,11 +1,23 @@
 import Data.List
+import Library
 
 distribute :: Int -> [Int] -> [[Int]]
 distribute n (x : []) = [[x + n]]
 distribute 0 l = [l]
-distribute n (x : xs) = concatMap (\i -> map (\y -> (x+i) : y) (distribute (n-i) xs)) [0..n]
+distribute n (x : xs) = concatMap (\i -> map (\ys -> (x+i) : ys) (distribute (n-i) xs)) [0..n]
 
 teaspoons = 100
- 
+
+combine amounts ingredients = let mix = map (zip ingredients) amounts
+                              in map (map (\(ingredient, amount) -> map (*amount) ingredient)) mix
+
+score = product . map (max 0) . map sum . transpose
+
 main = do
-  print $ distribute teaspoons (replicate 4 0)
+  input <- readFile "input/15.txt"
+  let ingredients = map (take 4 . readNumbers) (lines input)
+      base = (replicate (length ingredients) 0)
+      amounts = distribute teaspoons base
+      combinations = combine amounts ingredients
+
+  print $ maximum $ map score combinations
