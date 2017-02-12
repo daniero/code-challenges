9156fac7edf75eb0775fedf3c9796d97dcbfa9d4
Daniel Rødskog <danielr@mesan.no>
2017-02-12 01:29:34 +0100

    Refactor scoring and mapping of ingredients
    Make sure the fifth property, calories, is included in the ingredients
    list, but still not counted in the score.

--- aoc2015/15.hs
+++ aoc2015/15.hs
@@ -11,12 +11,13 @@ teaspoons = 100
 combine amounts ingredients = let mix = map (zip ingredients) amounts
                               in map (map (\(ingredient, amount) -> map (*amount) ingredient)) mix
 
-score = product . map (max 0) . map sum . transpose
+sumProperties = map (max 0 . sum) . transpose
+score = product . take 4 . sumProperties
 
 main = do
   input <- readFile "input/15.txt"
-  let ingredients = map (take 4 . readNumbers) (lines input)
-      base = (replicate (length ingredients) 0)
+  let ingredients = map readNumbers $ lines input
+      base = replicate (length ingredients) 0
       amounts = distribute teaspoons base
       combinations = combine amounts ingredients
 
