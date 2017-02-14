2d3236e8ed9d9d42dcdc77961b65fd99f7295328
Daniel Rødskog <danielr@mesan.no>
2017-02-14 19:24:06 +0100

    Solve AOC2015 day 18 part 1 in Haskell

---
+++ aoc2015/18.hs
@@ -0,0 +1,28 @@
+import Data.List
+import Library
+
+limit = 100
+
+neighbours x y = [ (a, b) | i <- [-1..1],
+                            j <- [-1..1],
+                            i /= 0 || j /= 0,
+                            let a = x + i,
+                            let b = y + j,
+                            0 <= a && a < limit,
+                            0 <= b && b < limit ]
+
+light m x y = let n = length $ filter (\(i,j) -> m !! j !! i) $ neighbours x y
+                  l = m !! y !! x
+               in if l
+                  then n == 2 || n == 3
+                  else n == 3
+
+tick m = [ [ light m x y | x <- [0..limit-1] ] | y <- [0..limit-1] ]
+
+main = do
+  input <- readFile "input/18.txt"
+  let l = lines input
+  let m = map (map (== '#')) l
+
+  let part1 = (times 100 tick) m
+  print $ length $ concatMap (filter (== True)) part1
