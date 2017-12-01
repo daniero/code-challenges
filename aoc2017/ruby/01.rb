29b942c766371fb7b6954887effc7f2ff03b3cbb
Daniel Rødskog <danielmero@gmail.com>
2017-12-01 19:58:21 +0100

    Solve AoC2017 day 1 in Ruby

---
+++ aoc2017/ruby/01.rb
@@ -0,0 +1,11 @@
+input = File.read('../input01.txt').scan(/\d/).map(&:to_i)
+
+# Part 1
+p input.zip(input.rotate)
+  .select { |a,b| a == b }
+  .sum(&:first)
+
+# Part 2
+p input.zip(input.rotate(input.size/2))
+  .select { |a,b| a == b }
+  .sum(&:first)
