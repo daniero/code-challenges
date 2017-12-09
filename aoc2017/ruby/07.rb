9de98bae009f6dbcbae3e7fa040919fcc02fa315
Daniel Rødskog <danielmero@gmail.com>
2017-12-09 01:19:31 +0100

    Add Aoc2017 day 7 part 2 solution

--- aoc2017/ruby/07.rb
+++ aoc2017/ruby/07.rb
@@ -5,8 +5,38 @@ input = File.read('../input07.txt')
             }
 
 
+parents = input.flat_map { |parent, _, children| children.map { |child| [child, parent] } }.to_h
+
 # Part 1
-children, parents = (input.flat_map { |parent, _, children| children.map { |child| [child, parent] } }).transpose
+root = (input.map(&:first) - parents.keys).first
+puts root
 
-puts (parents - children).uniq
+# Part 2
 
+Node = Struct.new(:name, :weight, :children) do
+  def total_weight
+    weight + children.sum(&:total_weight)
+  end
+end
+
+nodes = Hash.new { |h,name| h[name] = Node.new(name, nil, []) }
+
+input.each { |name, weight, children|
+  node = nodes[name]
+  node.weight = weight
+  node.children+= children.map { |child| nodes[child] }
+}
+
+# Manual inspection is faster:
+
+puts
+p nodes[root].total_weight
+p nodes[root].children.map(&:total_weight)
+p nodes[root].children.last.weight
+p nodes[root].children.last.children.map(&:total_weight)
+p nodes[root].children.last.children[2].children.map(&:total_weight)
+p nodes[root].children.last.children[2].children[3].children.map(&:total_weight)
+p nodes[root].children.last.children[2].children[3].weight
+puts
+
+p 1531-(1531-(2255-243*3))
