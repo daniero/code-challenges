a0339796e772043cf3a15118af18cd2ab1144cc1
Daniel Rødskog <danielmero@gmail.com>
2017-12-16 15:40:33 +0100

    Extract dance method

--- aoc2017/ruby/16.rb
+++ aoc2017/ruby/16.rb
@@ -1,19 +1,21 @@
-input = File.read('../input16.txt').split(',')
-
-programs = [*'a'..'p']
-
-input.each do |move|
-  case move
-  when /s(\d+)/
-    programs.rotate! -$1.to_i
-  when /x(\d+)\/(\d+)/
-    a,b = $1.to_i, $2.to_i
-    programs[a], programs[b] = programs[b], programs[a]
-  when /p(.)\/(.)/
-    i = programs.index $1
-    j = programs.index $2
-    programs[i], programs[j] = programs[j], programs[i]
+def dance(programs, moves)
+  moves.each do |move|
+    case move
+    when /s(\d+)/
+      programs.rotate! -$1.to_i
+    when /x(\d+)\/(\d+)/
+      a,b = $1.to_i, $2.to_i
+      programs[a], programs[b] = programs[b], programs[a]
+    when /p(.)\/(.)/
+      i = programs.index $1
+      j = programs.index $2
+      programs[i], programs[j] = programs[j], programs[i]
+    end
   end
 end
 
+input = File.read('../input16.txt').split(',')
+
+programs = [*'a'..'p']
+dance(programs, input)
 puts programs.join
