2920d33d9e5cd4685ad8e1ce31456fc6b3e21ad1
Daniel Rødskog <danielmero@gmail.com>
2017-12-23 17:17:43 +0100

    Generate code for part 2

--- aoc2017/ruby/23.rb
+++ aoc2017/ruby/23.rb
@@ -1,27 +1,31 @@
+PART = 2
+
 def label(i, statements, goto_next=true)
   "case #{i}: #{statements}; #{'go++;' if goto_next}"
 end
 
-puts (?a..?h).map { |c| "long #{c} = 0;" }
-
-puts <<BOILERPLATE
-
-int go = 0;
-int mul = 0;
-
-while(true)
-switch (go) {
-BOILERPLATE
-
 input = File.readlines('../input23.txt')
 
-puts input.each_with_index.map { |line, i|
-  line
-    .gsub(/set (.) (-?\w+)/) { label(i, "#$1 = #$2") }
-    .gsub(/sub (.) (-?\w+)/) { label(i, "#$1-= #$2") }
-    .gsub(/mul (.) (-?\w+)/) { label(i, "mul++; #$1*= #$2") }
-    .gsub(/jnz 1 (-?\w+)/) {  label(i, "go = #{i + $1.to_i}; break", false) }
-    .gsub(/jnz (.) (-?\w+)/) { label(i, "if (#$1 != 0) { go = #{i + $2.to_i}; break; }") }
-}
+if PART == 1
+  puts (?a..?h).map { |c| "long #{c} = 0;" }
 
-puts "}"
+  puts input.each_with_index.map { |line, i|
+    line
+      .gsub(/set (.) (-?\w+)/) { label(i, "#$1 = #$2") }
+      .gsub(/sub (.) (-?\w+)/) { label(i, "#$1-= #$2") }
+      .gsub(/mul (.) (-?\w+)/) { label(i, "mul++; #$1*= #$2") }
+      .gsub(/jnz 1 (-?\w+)/) {  label(i, "go = #{i + $1.to_i}; break", false) }
+      .gsub(/jnz (.) (-?\w+)/) { label(i, "if (#$1 != 0) { go = #{i + $2.to_i}; break; }") }
+  }
+elsif PART == 2
+  puts (?b..?h).map { |c| "long #{c} = 0;" }
+
+  puts input.each_with_index.map { |line, i|
+    line
+      .gsub(/set (.) (-?\w+)/) { label(i, "#$1 = #$2") }
+      .gsub(/sub (.) (-?\w+)/) { label(i, "#$1-= #$2") }
+      .gsub(/mul (.) (-?\w+)/) { label(i, "#$1*= #$2") }
+      .gsub(/jnz [a1] (-?\w+)/) {  label(i, "go = #{i + $1.to_i}; break", false) }
+      .gsub(/jnz (.) (-?\w+)/) { label(i, "if (#$1 != 0) { go = #{i + $2.to_i}; break; }") }
+  }
+end
