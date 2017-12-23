e651e28ed45c7d705f5bb443bd1aa39b62e2a726
Daniel Rødskog <danielmero@gmail.com>
2017-12-23 16:48:17 +0100

    Fixup part 1

--- aoc2017/ruby/23.rb
+++ aoc2017/ruby/23.rb
@@ -1,18 +1,27 @@
-def label(i, statements)
-  "case #{i}: #{statements}; go++;"
+def label(i, statements, goto_next=true)
+  "case #{i}: #{statements}; #{'go++;' if goto_next}"
 end
 
 puts (?a..?h).map { |c| "long #{c} = 0;" }
 
+puts <<BOILERPLATE
+
+int go = 0;
+int mul = 0;
+
+while(true)
+switch (go) {
+BOILERPLATE
+
 input = File.readlines('../input23.txt')
 
 puts input.each_with_index.map { |line, i|
-  java =
-    line
-    .gsub(/set (.) (-?\w+)/) { "#$1 = #$2" }
-    .gsub(/sub (.) (-?\w+)/) { "#$1-= #$2" }
-    .gsub(/mul (.) (-?\w+)/) { "mul++; #$1*= #$2" }
-    .gsub(/jnz (.) (-?\w+)/) { "if (#$1 != 0) { go+= #$2; break; }" }
-  label(i, java.chomp)
+  line
+    .gsub(/set (.) (-?\w+)/) { label(i, "#$1 = #$2") }
+    .gsub(/sub (.) (-?\w+)/) { label(i, "#$1-= #$2") }
+    .gsub(/mul (.) (-?\w+)/) { label(i, "mul++; #$1*= #$2") }
+    .gsub(/jnz 1 (-?\w+)/) {  label(i, "go+= #$1; break", false) }
+    .gsub(/jnz (.) (-?\w+)/) { label(i, "if (#$1 != 0) { go+= #$2; break; }") }
 }
 
+puts "}"
