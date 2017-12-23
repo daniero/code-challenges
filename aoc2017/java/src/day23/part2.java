2b9e7a74dfd254ed9ab55895013a3a46a2c18ae8
Daniel Rødskog <danielmero@gmail.com>
2017-12-23 18:07:35 +0100

    Cleanup generated code
    Removed case labels that are not jumped to.
    Removed all instances of `GOTO++`.

--- aoc2017/java/src/day23/part2.java
+++ aoc2017/java/src/day23/part2.java
@@ -7,9 +7,9 @@ class Part2 {
 
         b = 79;
         b *= 100;
-        b-= -100000;
+        b -= -100000;
         c = b;
-        c-= -17000;
+        c -= -17000;
 
         long d = 0;
         long e = 0;
@@ -22,30 +22,42 @@ class Part2 {
         while (true) {
             switch (GOTO) {
                 // @formatter:off
-                case 8: f = 1; GOTO++;
-                case 9: d = 2; GOTO++;
-                case 10: e = 2; GOTO++;
-                case 11: g = d; GOTO++;
-                case 12: g*= e; GOTO++;
-                case 13: g-= b; GOTO++;
-                case 14: if (g != 0) { GOTO = 16; break; }; GOTO++;
-                case 15: f = 0; GOTO++;
-                case 16: e-= -1; GOTO++;
-                case 17: g = e; GOTO++;
-                case 18: g-= b; GOTO++;
-                case 19: if (g != 0) { GOTO = 11; break; }; GOTO++;
-                case 20: d-= -1; GOTO++;
-                case 21: g = d; GOTO++;
-                case 22: g-= b; GOTO++;
-                case 23: if (g != 0) { GOTO = 10; break; }; GOTO++;
-                case 24: if (f != 0) { GOTO = 26; break; }; GOTO++;
-                case 25: h-= -1; GOTO++;
-                case 26: g = b; GOTO++;
-                case 27: g-= c; GOTO++;
-                case 28: if (g != 0) { GOTO = 30; break; }; GOTO++;
-                case 29: GOTO = 32; break;
-                case 30: b-= -17; GOTO++;
-                case 31: GOTO = 8; break;
+                case 8:
+                    f = 1;
+                    d = 2;
+
+                case 10:
+                    e = 2;
+
+                case 11:
+                    g = d;
+                    g*= e;
+                    g-= b;
+                    if (g != 0) { GOTO = 16; break; }
+                    f = 0;
+
+                case 16:
+                    e-= -1;
+                    g = e;
+                    g-= b;
+                    if (g != 0) { GOTO = 11; break; }
+                    d-= -1;
+                    g = d;
+                    g-= b;
+                    if (g != 0) { GOTO = 10; break; }
+                    if (f != 0) { GOTO = 26; break; }
+                    h-= -1;
+
+                case 26:
+                    g = b;
+                    g-= c;
+                    if (g != 0) { GOTO = 30; break; }
+                    GOTO = 32; break;
+
+                case 30:
+                    b-= -17;
+                    GOTO = 8; break;
+
                 // @formatter:on
                 default:
                     return h;
