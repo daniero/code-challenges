d83fd7fe68c40e34f5719ae2c166c7fd89c23424
Daniel Rødskog <danielmero@gmail.com>
2017-12-23 21:17:15 +0100

    Replace do-while with for loop and break out early

--- aoc2017/java/src/day23/part2.java
+++ aoc2017/java/src/day23/part2.java
@@ -4,40 +4,25 @@ class Part2 {
     static long part2() {
         final long C = 124900;
 
-        long b = 107900;
-
-        long d = 0;
-        long e = 0;
-        long f = 0;
         long h = 0;
 
-        do {
-            System.out.printf("b=%d, d=%d, e=%d, f=%d, h=%d\n", b, d, e, f, h);
-            f = 1;
-            d = 2;
+        for (long b = 107900; b <= C; b += 17) {
+            long f = 1;
 
-            do {
-                e = 2;
+            d:
+            for (long d = 2; d < b; d++) {
+                for (long e = 2; e < b; e++) {
+                    if (d * e == b) {
+                        f = 0;
+                        break d;
+                    }
+                }
+            }
 
-                do {
-                    if (d*e == b) { f = 0; }
-
-                    e++;
-                } while (e != b);
-
-                d++;
-            } while (d != b);
-
-            if (f == 0) { h++; }
-
-            if (b == C) { return h; }
-
-            b += 17;
-        } while (true);
+            if (f == 0) {
+                h++;
+            }
+        }
+        return h;
     }
 }
-
-// Is it lcm?
-// Nope: 107900.lcm(124900) => 134767100 => too high
-// Is it gcd?
-// Nope: 124900.gcd 107900 => 100 => too low
