4d931b46bf0fb6ff49ad5b0df56916e2a55f1567
Daniel Rødskog <danielmero@gmail.com>
2017-12-25 15:54:11 +0100

    Generate Ruby code from input and evaluate it

--- aoc2017/ruby/25.rb
+++ aoc2017/ruby/25.rb
@@ -1,8 +1,9 @@
 class TuringMachine
+  attr_reader :checksum_steps, :tape
+
   def initialize()
     @tape = []
     @cursor = 0
-    @state = :a
   end
 
   def read
@@ -29,3 +30,18 @@ class TuringMachine
     @state = self.method(@state).call
   end
 end
+
+instructions = File.read('../input25.txt')
+  .downcase
+  .gsub(/begin in state (\w)./) { "@state = :#$1" }
+  .gsub(/perform a diagnostic checksum after (\d+) steps.\n/) { "@checksum_steps = #$1" }
+  .gsub(/in state (\w):/) { "def #$1" }
+  .gsub(/if .* 0:/) { "if read == 0" }
+  .gsub(/if .* 1:/) { "else" }
+  .gsub(/- write the value (\d)./) { "write #$1" }
+  .gsub(/- move one slot to the (\w+)./) { "move_#$1" }
+  .gsub(/- continue with state (\w)./) { ":#$1" }
+  .gsub(/^$|\z$/) { "  end\nend\n" }
+
+tm = TuringMachine.new
+tm.instance_eval instructions
