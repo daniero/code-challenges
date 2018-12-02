bbb3caaa9a5cbdfea97db622bda7ac9958cfd730
Daniel Rødskog <danielmero@gmail.com>
2018-12-02 20:55:06 +0100

    Solve Aoc 2018 day 2 in Ruby

--- aoc2018/README.md
+++ aoc2018/README.md
@@ -4,3 +4,7 @@ http://adventofcode.com/2018
 
 **Day 01:**
 [Ruby](ruby/01.rb)ᵃᵇ
+
+**Day 02:**
+[Ruby](ruby/02.rb)ᵃᵇ
+
---
+++ aoc2018/ruby/02.rb
@@ -0,0 +1,28 @@
+box_ids = File.readlines('../input/input02.txt').map(&:chomp)
+
+# Part 1
+
+letter_counts = box_ids.map do |line|
+  ('a'..'z').map { |letter| line.count(letter) }
+end
+
+twos = letter_counts.count { |counts| counts.member? 2 }
+threes = letter_counts.count { |counts| counts.member? 3 }
+
+puts twos * threes
+
+
+# Part 2
+
+correct_ids = box_ids.product(box_ids).find do |id1, id2|
+  diffs = id1.chars.zip(id2.chars).count { |a,b| a != b }
+  diffs == 1
+end
+
+puts correct_ids
+  .map(&:chars)
+  .transpose
+  .select { |a,b| a == b }
+  .transpose
+  .first
+  .join
