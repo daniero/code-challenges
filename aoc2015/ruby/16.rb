17ec36762afaadd46addcc53ef430d2dafae3d23
Daniel Rødskog <danielmero@gmail.com>
2018-12-02 02:44:51 +0100

    Solve AoC 2015 day 16 part 2 in Ruby

--- aoc2015/ruby/16.rb
+++ aoc2015/ruby/16.rb
@@ -6,7 +6,17 @@ aunts =
       .to_h
   end
 
-sender = {
+def find_aunt(sender, aunts)
+  aunts.index { |aunt|
+    sender.all? { |prop, count|
+      aunt[prop].nil? || count === aunt[prop]
+    }
+  } + 1
+end
+
+
+# Part 1
+sender1 = {
   children: 3,
   cats: 7,
   samoyeds: 2,
@@ -18,11 +28,19 @@ sender = {
   cars: 2,
   perfumes: 1,
 }
+p find_aunt(sender1, aunts)
 
-# Part 1
-
-p aunts.index { |aunt|
-  sender.all? { |prop, count|
-    aunt[prop].nil? || aunt[prop] == count
-  }
-} + 1
+# Part 2
+sender2 = {
+  children: 3,
+  cats: (8..100),
+  samoyeds: 2,
+  pomeranians: (0...3),
+  akitas: 0,
+  vizslas: 0,
+  goldfish: (0...5),
+  trees: (4..100),
+  cars: 2,
+  perfumes: 1,
+}
+p find_aunt(sender2, aunts)
