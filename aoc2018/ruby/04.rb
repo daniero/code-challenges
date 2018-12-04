213afeb882a0e454c0a9b0e83797dbfdaaae5f56
Daniel Rødskog <danielmero@gmail.com>
2018-12-04 22:05:52 +0100

    Solve AoC 2018 day 4 part 1 in Ruby

---
+++ aoc2018/ruby/04.rb
@@ -0,0 +1,44 @@
+require 'date'
+
+def minutes_asleep(changes)
+  [0, *changes, 60]
+    .each_cons(2)
+    .with_index
+    .flat_map { |(t1, t2), i| (t1...t2).map { i%2 } }
+end
+
+def sum_sleep_pr_minute_pr_guard(input)
+  shifts = input
+    .slice_before { |_, event| event =~ /begins shift/ }
+    .map { |(_, start), *sleep_changes|
+      guard_id = start[/(?<=Guard #)\d+/].to_i
+      minutes = sleep_changes.map(&:first)
+      [guard_id, minutes_asleep(minutes)]
+    }
+
+  guard_ids = shifts.map(&:first).uniq
+
+  sum_sleep_pr_minute =
+    guard_ids.map do |guard_id|
+      shifts
+        .select { |id, _| id == guard_id }
+        .map(&:last)
+        .transpose
+        .map(&:sum)
+    end
+
+  return [guard_ids, sum_sleep_pr_minute].transpose.to_h
+end
+
+
+input = File.readlines('../input/input04.txt')
+  .sort
+  .flat_map { |line| line.scan /\[(.*)\] (.*)/  }
+  .map { |timestamp, event| [DateTime.parse(timestamp).minute, event] }
+
+sleep_pr_guard = sum_sleep_pr_minute_pr_guard(input)
+
+id, sleep = sleep_pr_guard.max_by { |id, sleep| sleep.sum }
+minute = sleep.index(sleep.max)
+
+p id * minute
