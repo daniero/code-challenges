dcc0adea893aabef4848a71ced7b6719d1ab8791
Daniel Rødskog <danielmero@gmail.com>
2018-12-14 13:11:30 +0100

    Solve AoC 2018 day 14 part 2 in Ruby
    Allocate necessary space for recipy scores only once, instead of
    growing the array on each iteration.

--- aoc2018/ruby/14.rb
+++ aoc2018/ruby/14.rb
@@ -1,33 +1,55 @@
-INPUT = 793031
+def find_recipes
+  scores = [3, 7]
+  elf1 = 0
+  elf2 = 1
 
-scores = [3, 7]
-elf1 = 0
-elf2 = 1
+  loop do
+    score1, score2 = scores.values_at(elf1, elf2)
 
-LIMIT = INPUT + 10
-recipies_discovered = scores.size
-scores += [-1] * (LIMIT - recipies_discovered)
+    sum = score1 + score2
+    if sum >= 10
+      recipe1, recipe2 = sum.divmod(10)
+      scores.push(recipe1)
+      break if yield scores.size, recipe1
+      scores.push(recipe2)
+      break if yield scores.size, recipe2
+    else
+      scores.push(sum)
+      break if yield scores.size, sum
+    end
 
-i = 0
-while recipies_discovered < LIMIT do
-  print "Iteration: #{i+=1}, Recipies: #{recipies_discovered}\r"
 
-  score1, score2 = scores.values_at(elf1, elf2)
+    elf1 = (elf1 + score1 + 1) % scores.size
+    elf2 = (elf2 + score2 + 1) % scores.size
+  end
 
-  # puts scores
-  #   .map
-  #   .with_index { |score, index| index == elf1 ? "(#{score})" : index == elf2 ? "[#{score}]" : " #{score} " }
-  #   .join(' ')
-
-  new_recipies = (score1 + score2).digits.reverse
-  scores[recipies_discovered, new_recipies.length] = new_recipies
-  recipies_discovered += new_recipies.length
-
-  elf1 += score1 + 1
-  elf1 %= recipies_discovered
-  elf2 += score2 + 1
-  elf2 %= recipies_discovered
+  return scores, scores.size
 end
 
-puts "\n"+ scores.last(10).join
+INPUT = 793031
 
+# Part 1
+
+N = 10
+LIMIT = INPUT + N
+
+scores, total_amount = find_recipes { |current_amount, _|
+  current_amount == LIMIT 
+}
+
+puts "Part 1: " + scores[total_amount-N, N].join
+
+# Part 2
+
+magnitude = 10 ** INPUT.digits.length
+buffer = 0
+
+find_recipes do |current_amount, score|
+  buffer = (buffer * 10) % magnitude + score
+
+  if buffer == INPUT
+    answer = current_amount - INPUT.digits.length
+    puts "Part 2: #{answer}"
+    true
+  end
+end
