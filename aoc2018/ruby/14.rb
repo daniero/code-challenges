ff9cd82b2b4ceeec44a05d1da1b1a8f3c9797531
Daniel Rødskog <danielmero@gmail.com>
2018-12-14 13:11:30 +0100

    Optimize slow solution
    Allocate necessary space for recipy scores only once, instead of
    growing the array on each iteration.

--- aoc2018/ruby/14.rb
+++ aoc2018/ruby/14.rb
@@ -1,30 +1,33 @@
-require 'set'
-
 INPUT = 793031
 
 scores = [3, 7]
 elf1 = 0
 elf2 = 1
 
+LIMIT = INPUT + 10
+recipies_discovered = scores.size
+scores += [-1] * (LIMIT - recipies_discovered)
+
 i = 0
-while scores.size < INPUT + 10 do
-  i += 1
-  print "Iterations: #{i}, Recipies: #{scores.size}\r"
-  index1 = elf1 % scores.size
-  index2 = elf2 % scores.size
-  score1, score2 = scores.values_at(index1, index2)
+while recipies_discovered < LIMIT do
+  print "Iteration: #{i+=1}, Recipies: #{recipies_discovered}\r"
+
+  score1, score2 = scores.values_at(elf1, elf2)
 
   # puts scores
   #   .map
   #   .with_index { |score, index| index == elf1 ? "(#{score})" : index == elf2 ? "[#{score}]" : " #{score} " }
   #   .join(' ')
 
-  scores += (score1 + score2).digits.reverse
+  new_recipies = (score1 + score2).digits.reverse
+  scores[recipies_discovered, new_recipies.length] = new_recipies
+  recipies_discovered += new_recipies.length
 
   elf1 += score1 + 1
-  elf1 %= scores.size
+  elf1 %= recipies_discovered
   elf2 += score2 + 1
-  elf2 %= scores.size
+  elf2 %= recipies_discovered
 end
 
 puts "\n"+ scores.last(10).join
+
