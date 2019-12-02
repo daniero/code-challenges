7dea808bcfd24cc958fee79570a1df4886c185b3
Daniel Rødskog <danielmero@gmail.com>
2019-12-02 18:17:14 +0100

    Solve AoC 1029 day 2 part 2 in Ruby

--- aoc2019/02.rb
+++ aoc2019/02.rb
@@ -1,18 +1,39 @@
 input = File.read('input/input02.txt').scan(/\d+/).map(&:to_i)
 
-input[1] = 12
-input[2] = 2
+def run(program, noun, verb)
+  memory = program.dup
 
-input
-  .each_slice(4) { |a,b,c,d|
-    case a
+  memory[1] = noun
+  memory[2] = verb
+
+  memory.each_slice(4) { |opcode, n1, n2, target|
+    case opcode
     when 1
-      input[d] = input[b] + input[c]
+      memory[target] = memory[n1] + memory[n2]
     when 2
-      input[d] = input[b] * input[c]
+      memory[target] = memory[n1] * memory[n2]
     when 99
       break
     end
   }
 
-p input[0]
+  return memory[0]
+end
+
+
+# Part 1
+
+p run(input, 12, 2)
+
+
+# Part 2
+
+output = 19690720
+
+possible_inputs = [*0..99]
+combinations = possible_inputs.repeated_permutation(2)
+
+p combinations
+  .find { |noun, verb| run(input, noun, verb) == output }
+  .then  { |noun, verb| 100 * noun + verb }
+
