8c42202d313eca1888580784e6bb4f3e7a9cbecd
Daniel Rødskog <danielmero@gmail.com>
2019-12-05 19:20:18 +0100

    Implement parameter mode

--- aoc2019/ruby/intcode.rb
+++ aoc2019/ruby/intcode.rb
@@ -1,23 +1,66 @@
-class IntcodeComputer
-  attr_accessor :memory
+PositionMode = 0
+ImmediateMode = 1
 
-  def initialize(program, noun = nil, verb = nil)
+class IntcodeComputer
+  attr_accessor :memory, :ip
+
+  def initialize(program,
+                 noun: nil,
+                 verb: nil)
     @memory = program.dup
     memory[1] = noun || memory[1]
     memory[2] = verb || memory[2]
+    @ip = 0
+  end
+
+  def read_int
+    memory[ip]
+      .tap { @ip += 1 }
+  end
+
+  def read_instruction
+    opcode = read_int
+    e,d,c,b,a = opcode.digits
+
+    instruction = 10 * (d||0) + e
+    m1 = c || PositionMode
+    m2 = b || PositionMode
+    m3 = a || PositionMode
+
+    [instruction, m1, m2, m3]
+  end
+
+  def read_value(mode)
+    value = read_int
+    if mode == PositionMode
+      return memory[value]
+    else
+      return value
+    end
+  end
+
+  def write_value(value)
+    target = read_value(ImmediateMode)
+    memory[target] = value
   end
 
   def run()
-    memory.each_slice(4) { |opcode, n1, n2, target|
-      case opcode
+    while ip < memory.length
+      instruction, m1, m2, m3 = read_instruction
+
+      case instruction
       when 1
-        memory[target] = memory[n1] + memory[n2]
+        a = read_value(m1)
+        b = read_value(m2)
+        write_value(a + b)
       when 2
-        memory[target] = memory[n1] * memory[n2]
+        a = read_value(m1)
+        b = read_value(m2)
+        write_value(a * b)
       when 99
         break
       end
-    }
+    end
 
     return memory[0]
   end
