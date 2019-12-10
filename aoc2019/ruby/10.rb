913e20bc6fb719ee9535fd1f4ee52d94d55d5135
Daniel Rødskog <danielmero@gmail.com>
2019-12-10 23:47:37 +0100

    Solve part 2

--- aoc2019/ruby/10.rb
+++ aoc2019/ruby/10.rb
@@ -7,16 +7,30 @@ asteroids = File
       .each_char
       .with_index
       .flat_map { |char, charnum|
-        char == '#' ?  [[linenum, charnum]] : []
+        char == '#' ?  [[charnum, linenum]] : []
       }
   }
 
-def radians_between(x1, y1, x2, y2)
-  Math.atan2(x2-x1, y2-y1)
+def angle_between(x1, y1, x2, y2)
+  Math.atan2(x2-x1, -(y2-y1)) % (2*Math::PI)
 end
 
-pp asteroids.map { |asteroid|
-  others = asteroids - asteroid
-  in_line = others.group_by { |other| radians_between(*asteroid, *other) }
-  in_line.size
-}.max
+def distance_between(x1, y1, x2, y2)
+  Math.sqrt((x1-x2)**2 + (y1-y2)**2)
+end
+
+
+location, lines_of_sight = asteroids
+  .map { |asteroid|
+    others = asteroids - asteroid
+    lines = others.group_by { |other| angle_between(*asteroid, *other) }
+    [asteroid, lines]
+  }
+  .max_by { |_, lines| lines.size }
+
+p lines_of_sight.size
+
+p lines_of_sight
+  .sort[199].last
+  .min_by { |asteroid| distance_between(*location, *asteroid) }
+  .then { |x,y| x*100+y }
