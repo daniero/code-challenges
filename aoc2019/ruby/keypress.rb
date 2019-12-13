ee5757cf635c83dcb5e7179dc8d01e2ee4e37d9f
Daniel Rødskog <danielmero@gmail.com>
2019-12-13 22:07:31 +0100

    Steal script for fetching keypresses
    Credits: acook @ github: https://gist.github.com/acook/4190379

---
+++ aoc2019/ruby/keypress.rb
@@ -0,0 +1,61 @@
+require 'io/console'
+
+# Reads keypresses from the user including 2 and 3 escape character sequences.
+def read_char
+  STDIN.echo = false
+  STDIN.raw!
+
+  input = STDIN.getc.chr
+  if input == "\e" then
+    input << STDIN.read_nonblock(3) rescue nil
+    input << STDIN.read_nonblock(2) rescue nil
+  end
+ensure
+  STDIN.echo = true
+  STDIN.cooked!
+
+  return input
+end
+
+# oringal case statement from:
+# http://www.alecjacobson.com/weblog/?p=75
+def show_single_key
+  c = read_char
+
+  case c
+  when " "
+    puts "SPACE"
+  when "\t"
+    puts "TAB"
+  when "\r"
+    puts "RETURN"
+  when "\n"
+    puts "LINE FEED"
+  when "\e"
+    puts "ESCAPE"
+  when "\e[A"
+    puts "UP ARROW"
+  when "\e[B"
+    puts "DOWN ARROW"
+  when "\e[C"
+    puts "RIGHT ARROW"
+  when "\e[D"
+    puts "LEFT ARROW"
+  when "\177"
+    puts "BACKSPACE"
+  when "\004"
+    puts "DELETE"
+  when "\e[3~"
+    puts "ALTERNATE DELETE"
+  when "\u0003"
+    puts "CONTROL-C"
+    exit 0
+  when /^.$/
+    puts "SINGLE CHAR HIT: #{c.inspect}"
+  else
+    puts "SOMETHING ELSE: #{c.inspect}"
+  end
+end
+
+show_single_key while(true)
+
