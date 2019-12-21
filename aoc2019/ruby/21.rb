013b088274bc22dd1d92f8d0249484b6ec73ded4
Daniel Rødskog <danielmero@gmail.com>
2019-12-21 15:25:31 +0100

    Clean up

--- aoc2019/ruby/21.rb
+++ aoc2019/ruby/21.rb
@@ -1,14 +1,19 @@
 require_relative 'intcode'
 
-program = read_intcode('../input/input21.txt')
+def run_springcode(intcode, springcode)
+  output = IntcodeComputer.new(
+    intcode,
+    input: springcode.chars.map(&:ord)
+  ).run.output
 
-puts IntcodeComputer.new(
-  program,
-  input: DATA.read.chars.map(&:ord)
-).run.output.map(&:chr).join
+  puts output.map { |int| int > 127 ? int.to_s : int.chr }.join
+end
 
 
-__END__
+intcode = read_intcode('../input/input21.txt')
+
+puts "== PART 1 =="
+run_springcode(intcode, <<END)
 NOT A J
 NOT B T
 OR T J
@@ -16,4 +21,4 @@ NOT C T
 OR T J
 AND D J
 WALK
-
+END
