f0d948ff960560f11df8214b022f889d6b4bf099
Daniel Rødskog <danielmero@gmail.com>
2020-12-02 16:36:04 +0100

    Solve AoC 2020 day 1 part 2 in Ruby

--- aoc2020/ruby/01.rb
+++ aoc2020/ruby/01.rb
@@ -1,6 +1,13 @@
-File
+input = File
   .readlines('../input/01.txt')
   .map(&:to_i)
+
+puts input
   .combination(2)
   .find { |a,b| a + b === 2020 }
-  .then { |a,b| puts a * b }
+  .then { |a,b| a * b }
+
+puts input
+  .combination(3)
+  .find { |a,b,c| a + b + c === 2020 }
+  .then { |a,b,c| a * b * c }
