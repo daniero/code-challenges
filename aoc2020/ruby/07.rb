8a58f9ea2b55120679336b36c98347d19becc837
Daniel Rødskog <danielmero@gmail.com>
2020-12-08 00:46:54 +0100

    Solve AoC 2020 day 7 part 2 in Ruby

--- aoc2020/ruby/07.rb
+++ aoc2020/ruby/07.rb
@@ -6,7 +6,9 @@ bags = File
     [container, containees]
   }.to_h
 
-  
+
+# Part 1
+
 def can_hold?(container, target, all_bags)
   bag = all_bags[container].map(&:last)
   return false unless bag
@@ -18,3 +20,14 @@ end
 p bags.keys.count { |bag| can_hold?(bag, 'shiny gold', bags) }
 
 
+# Part 2
+
+def must_hold(bag, all_bags)
+  content = all_bags[bag]
+
+  content.sum { |number, name|
+    number.to_i + number.to_i * must_hold(name, all_bags)
+  }
+end
+
+p must_hold('shiny gold', bags)
