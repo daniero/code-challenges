21db33c8756e77e55a0d8843c217d94b8932577f
Daniel Rødskog <danielmero@gmail.com>
2020-12-08 19:59:49 +0100

    Solve AoC 2020 day 8 part 2 in Ruby

--- aoc2020/ruby/08.rb
+++ aoc2020/ruby/08.rb
@@ -5,29 +5,53 @@ instructions = File
   .map { |line|
     line =~ /(\w+) ([+-]\d+)/
     [$1, $2.to_i]
-  }
+}
 
-ip = 0;
-acc = 0;
+def run_until_loop_or_terminate(instructions)
+  ip = 0;
+  acc = 0;
 
-visited = Set.new
+  visited = Set[]
 
-while ip >= 0 && ip < instructions.length
-  break unless visited.add?(ip)
+  loop do
+    return [false, acc] unless visited.add?(ip)
+    return [true, acc] if 0 > ip || ip >= instructions.size
 
-  inst, arg = instructions[ip]
-  case inst
-  when 'acc'
-    acc += arg
-    ip += 1
-  when 'jmp'
-    ip += arg
-  when 'nop'
-    ip += 1
-  else
-    raise "Unknow instruction @ ${ip}: '${inst} ${arg}'"
+    inst, arg = instructions[ip]
+    case inst
+    when 'acc'
+      acc += arg
+      ip += 1
+    when 'jmp'
+      ip += arg
+    when 'nop'
+      ip += 1
+    else
+      raise "Unknow instruction @ #{ip}: '#{inst} #{arg}'"
+    end
+
   end
-
 end
 
-puts acc
+
+# Part 1
+
+_, result = run_until_loop_or_terminate(instructions)
+puts result
+
+
+# Part 2
+
+instructions.each_with_index do |(inst, arg), index|
+  next unless inst == 'jmp' || inst == 'nop'
+
+  new_instructions = instructions.dup
+  new_instructions[index] = ['jmpnop'.sub(inst,''), arg]
+
+  terminated, result = run_until_loop_or_terminate(new_instructions)
+
+  if terminated
+    puts result
+    break
+  end
+end
