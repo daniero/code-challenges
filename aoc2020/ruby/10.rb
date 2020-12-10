5c89b4a6ab201559f1b591c1d4d44e8f07643fd2
Daniel Rødskog <danielmero@gmail.com>
2020-12-10 16:00:01 +0100

    Solve AoC 2020 day 10 part 2 in Ruby

--- aoc2020/ruby/10.rb
+++ aoc2020/ruby/10.rb
@@ -1,13 +1,39 @@
-input = File
+adapters = File
   .readlines('../input/10.txt')
   .map(&:to_i)
-
-
-jumps = [0, *input, input.max+3]
   .sort
+
+outlet = 0
+device = adapters.max + 3
+
+
+# Part 1
+
+jumps = [outlet, *adapters, device]
   .each_cons(2)
   .map { |a,b| b-a }
   .tally
   
 p jumps[1] * jumps[3]
 
+
+# Part 2
+
+require 'set'
+nodes = Set[outlet, *adapters, device]
+
+paths = Hash.new { 0 }
+paths[outlet] = 1
+
+possible_jumps = [1, 2, 3]
+
+[outlet, *adapters].each do |node|
+  possible_jumps.each do |jump|
+    target = node + jump
+    if nodes.include? target
+      paths[target] += paths[node]
+    end
+  end
+end
+
+p paths[device]
