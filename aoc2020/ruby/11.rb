82da3970e0c4b58cd30d9898de3727155a48e2a2
Daniel Rødskog <danielmero@gmail.com>
2020-12-11 12:54:40 +0100

    Refactor and generalize for part 2

--- aoc2020/ruby/11.rb
+++ aoc2020/ruby/11.rb
@@ -1,41 +1,55 @@
-initial_seats = File
-  .read('../input/11.txt')
-  .split("\n")
-  .map(&:chars)
-
-def neighbours(map, x, y)
-  [
-   [x-1, y-1], [x, y-1], [x+1, y-1],
-   [x-1, y  ],           [x+1, y  ],
-   [x-1, y+1], [x, y+1], [x+1, y+1]
-  ]
-    .select { |i,j| j >= 0 && j < map.length && i >= 0 && i < map[j].length }
-    .map { |i,j| map[j][i] }
-end
-
 FLOOR = '.'
 EMPTY = 'L'
 OCCUPIED = '#'
 
-def tick(seats)
+def tick(seats, occupied_threshold)
   seats.each_with_index.map { |row, y|
     row.each_with_index.map { |seat, x|
       next FLOOR if seat == FLOOR
-      neighbours_occupied = neighbours(seats, x, y).count(OCCUPIED)
+      neighbours_occupied = yield(seats, x, y)
       next OCCUPIED if seat == EMPTY && neighbours_occupied == 0
-      next EMPTY if seat == OCCUPIED && neighbours_occupied >= 4
+      next EMPTY if seat == OCCUPIED && neighbours_occupied >= occupied_threshold
       seat
     }
   }
 end
 
-prev = nil
-next_round = initial_seats
+def find_stable_state(initial_state, occupied_threshold, &count_occupied_seats)
+  prev_state = nil
+  next_state = initial_state
 
-until prev == next_round
-  print '.'
-  prev, next_round = next_round, tick(next_round)
+  until prev_state == next_state
+    prev_state, next_state = next_state, tick(next_state, occupied_threshold, &count_occupied_seats)
+  end
+
+  return next_state.sum { |row| row.count(OCCUPIED) }
 end
 
-puts
-p next_round.sum { |row| row.count(OCCUPIED) }
+
+# Part 1
+
+DIRECTIONS = [
+ [-1, -1], [0, -1], [+1, -1],
+ [-1,  0],          [+1,  0],
+ [-1, +1], [0, +1], [+1, +1]
+]
+
+
+initial_seats = File
+  .read('../input/11.txt')
+  .split("\n")
+  .map(&:chars)
+
+
+p find_stable_state(initial_seats, 4) { |state, x, y|
+  DIRECTIONS
+    .map { |i,j| [i+x, j+y] }
+    .select { |i,j| j >= 0 && j < state.length && i >= 0 && i < state[j].length }
+    .map { |i,j| state[j][i] }
+    .count(OCCUPIED)
+}
+
+
+# Part 2
+
+
