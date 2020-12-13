5658eb6ba730b0bb6ca522f07f27b39ef3a04eec
Daniel Rødskog <danielmero@gmail.com>
2020-12-13 14:55:14 +0100

    Solve AoC 2020 day 13 part 2 in Ruby

--- aoc2020/ruby/13.rb
+++ aoc2020/ruby/13.rb
@@ -11,3 +11,25 @@ p buses
   .map { |bus| [bus, bus-start%bus] }
   .min_by { |bus, wait| wait }
   .then { |bus, wait| bus*wait }
+
+
+# Part 2
+
+buses_with_index = buses
+  .sort
+  .reverse
+  .map { |bus|
+    i = bus_table.index(bus)
+    idx = (bus - i) % bus
+    [bus, idx]
+  }
+
+t = 1
+s = 1
+
+buses_with_index.each do |id, idx|
+  t += s until t % id == idx
+  s *= id
+end
+
+puts t
