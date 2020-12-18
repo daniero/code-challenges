5cb56a72f5a25f20eb2ffb9fa6231f6e3ca1c804
Daniel Rødskog <danielmero@gmail.com>
2020-12-18 12:21:55 +0100

    Make it even hackier

--- aoc2020/ruby/18.rb
+++ aoc2020/ruby/18.rb
@@ -1,23 +1,17 @@
-class WeirdInt
-  attr_reader :value
+# Part 2
 
-  def initialize value
-    @value = value
+class Integer
+  def - m
+    self * m
   end
 
-  def + m
-    WeirdInt.new(@value * m.value)
-  end
-
-  def * m
-    WeirdInt.new(@value + m.value)
+  def / m
+    self + m
   end
 end
 
 p File
   .open(ARGV[0] || '../input/18.txt')
-  .map { |line| line.gsub(/(\d+)/) { "WeirdInt.new(#$1)" } }
-  .map { |line| eval line.tr('+*', '*+') }
-  .sum(&:value)
-
+  .map { |line| eval line.tr('+*', '/-') }
+  .sum
 
