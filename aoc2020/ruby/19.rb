a9a655c908e0aa2185415caecd30d31bf7b381c1
Daniel Rødskog <danielmero@gmail.com>
2020-12-19 21:04:31 +0100

    Solve AoC 2020 day 19 part 2 in Ruby

--- aoc2020/ruby/19.rb
+++ aoc2020/ruby/19.rb
@@ -1,5 +1,5 @@
 r, m = File
-  .read('../input/19.txt')
+  .read(ARGV[0] || '../input/19.txt')
   .split("\n\n")
   .map { |chunk| chunk.lines.map(&:chomp) }
 
@@ -10,20 +10,32 @@ rules =
       .gsub(' | ', '],[')
       .gsub(' ', ',')
   }
-  .join(",\n")
+  .join(',')
   .then { ?{ + _1 + ?} }
   .then { eval _1 }
 
-resolve = Hash.new do |h,k|
-  rule = rules[k].map { |subrule|
-    subrule.map { |subsubrule|
-      String === subsubrule ? subsubrule : h[subsubrule]
-    }.join
-  }.then { |res| res.length == 1 ? res.first : "(#{res.join('|')})" }
+def create_solver(rules)
+  Hash.new do |h,k|
+    rule = rules[k].map { |subrule|
+      subrule.map { |subsubrule|
+        String === subsubrule ? subsubrule : h[subsubrule]
+      }.join
+    }.then { |res| res.length == 1 ? res.first : "(#{res.join('|')})" }
 
-  h[k] = rule
+    h[k] = rule
+  end
 end
 
-accept = Regexp.new(?^+resolve[0]+?$)
+# Part 1
+solver = create_solver(rules)
+inital_rule = Regexp.new(?^+solver[0]+?$)
 
-p m.grep(accept).count
+p m.grep(inital_rule).count
+
+# Part 2
+solver = create_solver(rules)
+solver[8] = "(#{solver[42]})+"
+solver[11] = "(?<r>#{solver[42]}\\g<r>?#{solver[31]})"
+inital_rule = Regexp.new(?^+solver[0]+?$)
+
+p m.grep(inital_rule).count
