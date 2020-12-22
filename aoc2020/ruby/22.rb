d2b53a37a6005d6bdc28a807df96e2ba6670fc89
Daniel Rødskog <danielmero@gmail.com>
2020-12-22 13:47:43 +0100

    Solve AoC 2020 day 22 part 2 in Ruby

--- aoc2020/ruby/22.rb
+++ aoc2020/ruby/22.rb
@@ -1,8 +1,10 @@
 cards = File
-  .read('../input/22.txt')
+  .read(ARGV[0] || '../input/22.txt')
   .split("\n\n")
   .map { |chunk| chunk.lines.drop(1).map(&:to_i) }
 
+
+# Part 1
 
 players = cards.map(&:dup)
 
@@ -15,10 +17,50 @@ while players.none? &:empty?
   end
 end
 
-
 p players
   .reject(&:empty?)
   .first
+  .reverse
+  .each_with_index
+  .sum { |card, i| card * (i+1) }
+
+
+# Part 2
+
+require 'set'
+
+def recursive_combat(cards)
+  players = cards.map(&:dup)
+  visited_states = Set.new
+
+  while players.none? &:empty?
+    unless visited_states.add? players.map { |d| d.join(',') }.join(';')
+      return [0, players]
+    end
+
+    a,b = players.map(&:shift)
+    round_winner = nil
+
+    if a <= players[0].length && b <= players[1].length
+      round_winner,_ = recursive_combat([players[0].take(a), players[1].take(b)])
+    else
+      round_winner = a > b ? 0 : 1
+    end
+
+    if round_winner == 0
+      players[0].push a, b
+    else
+      players[1].push b, a
+    end
+  end
+
+  winner = 1 - players.index(&:empty?)
+  return [winner, players]
+end
+
+winner, cards = recursive_combat(cards)
+
+p cards[winner]
   .reverse
   .each_with_index
   .sum { |card, i| card * (i+1) }
