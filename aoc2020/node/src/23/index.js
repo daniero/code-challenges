60fd6409762165a68b54f4288ce1a3bac2b286d3
Daniel Rødskog <danielmero@gmail.com>
2020-12-23 22:02:27 +0100

    Refactor part 1 to reusable function

--- aoc2020/node/src/23/index.js
+++ aoc2020/node/src/23/index.js
@@ -1,16 +1,54 @@
-const input = 496138527;
-const moves = 100;
-// test:
-// const input = 389125467;
-// const moves = 10;
+function solve(start, moves) {
+  let current = start
+  let pickedUp;
+  let destination;
 
-const cups = [...input.toString()]
-  .map(c => parseInt(c))
-  .map(n => ({ label: n }));
+  function pickUp() {
+    const first = current.next;
+    const last = first.next.next;
+    current.next = last.next;
+    last.next = null;
+    pickedUp = first;
+  }
 
-cups.forEach((cup, i) => {
-  cup.next = cups[(i + 1) % cups.length];
-});
+  function findDestination() {
+    const pickedUpLabels = [pickedUp.label, pickedUp.next.label, pickedUp.next.next.label];
+    destination = current.label;
+    do {
+      destination--;
+      if (destination === 0) destination = 9;
+    } while (pickedUpLabels.includes(destination));
+  }
+
+  function placeBackDown() {
+    let next = current.next;
+    while (next.label !== destination) {
+      next = next.next;
+    }
+    const oldNext = next.next;
+    next.next = pickedUp;
+    pickedUp.next.next.next = oldNext;
+  }
+
+  function rotate() {
+    current = current.next;
+  }
+
+  for (let i = 1; i <= moves; i++) {
+    if (i % 100000 === 0) process.stdout.write(".");
+    pickUp();
+    findDestination();
+    placeBackDown()
+    rotate()
+  }
+  return current;
+}
+
+function rotateToCup1(cup) {
+  let current = cup;
+  while (current.label !== 1) current = current.next;
+  return current;
+}
 
 const print = (first) => {
   let next = first;
@@ -19,53 +57,52 @@ const print = (first) => {
     labels.push(next.label);
     next = next.next;
   } while (next && next !== first);
-  if (next) {
-    labels.push('...');
-  }
-  return labels.join(', ');
+  return labels.slice(1).join('');
 }
 
-let current = cups[0]
-let pickedUp;
-let destination;
+// const input = 389125467; //test
+const input = 496138527;
 
-function pickUp() {
-  const first = current.next;
-  const last = first.next.next;
-  current.next = last.next;
-  last.next = null;
-  pickedUp = first;
+// Part 1
+
+const cups1 = [...input.toString()]
+  .map(c => parseInt(c))
+  .map(n => ({ label: n }));
+
+cups1.forEach((cup, i) => {
+  cup.next = cups1[(i + 1) % cups1.length];
+});
+
+const part1 = solve(cups1[0], 100);
+
+console.log("Part 1:")
+console.log()
+console.log(`Cups: ${print(rotateToCup1(part1))}`)
+
+// Part 2
+
+const cups2 = [...input.toString()]
+  .map(c => parseInt(c))
+  .map(n => ({ label: n }));
+
+cups2.forEach((cup, i) => {
+  cup.next = cups2[(i + 1) % cups2.length];
+});
+
+const first = cups2[0];
+let next = cups2[cups2.length - 1];
+
+const oneMillion = 1000000;
+const hundredMillion = 100000000;
+
+for (let i = 10; i <= oneMillion; i++) {
+  next.next = { label: i };
+  next = next.next
 }
 
-function findDestination() {
-  const pickedUpLabels = [pickedUp.label, pickedUp.next.label, pickedUp.next.next.label];
-  destination = current.label;
-  do {
-    destination--;
-    if (destination === 0) destination = 9;
-  } while (pickedUpLabels.includes(destination));
-}
+next.next = first
 
-function placeBackDown() {
-  let next = current.next;
-  while (next.label !== destination) {
-    next = next.next;
-  }
-  const oldNext = next.next;
-  next.next = pickedUp;
-  pickedUp.next.next.next = oldNext;
-}
+console.log("Part 2:")
+const part2 = rotateToCup1(solve(first, hundredMillion));
 
-function rotate() {
-  current = current.next;
-}
-
-for (let i = 1; i <= moves; i++) {
-  pickUp();
-  findDestination();
-  placeBackDown()
-  rotate()
-}
-
-console.log("final:")
-console.log(`Cups: ${print(current)}`)
+console.log(part2.next * part2.next.next);
