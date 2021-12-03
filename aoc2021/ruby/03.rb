8f620a6c9f2fa0e535d43d60c1dd9126fa1a0aeb
Daniel Rødskog <danielmero@gmail.com>
2021-12-03 17:45:33 +0100

    Solve AOC2021 day 3 part 2 in Ruby

--- aoc2021/ruby/03.rb
+++ aoc2021/ruby/03.rb
@@ -3,12 +3,44 @@ input =
     .readlines('../input/03.txt')
     .map { |line| line.chomp.chars.map(&:to_i) }
 
-gamma_rate =
-  input
+def gamma(ns)
+  ns
     .transpose
     .map { |c| c.count(0) > c.count(1) ? 0 : 1 }
-    .join.to_i(2)
+end
 
-epsilon_rate = ~gamma_rate & ~(-1 << gamma_rate.bit_length)
+
+# Part 1
+
+gamma_rate = gamma(input).join.to_i(2)
+epsilon_rate = (~gamma_rate)[0...gamma_rate.bit_length]
 
 p gamma_rate * epsilon_rate
+
+
+# Part 2
+
+oxygen = input.dup
+i = 0
+until oxygen.length == 1
+  g = gamma(oxygen)
+  
+  oxygen.reject! { |line| line[i] != g[i] }
+
+  i += 1
+end
+
+
+scrubber = input.dup
+i = 0
+until scrubber.length == 1
+  g = gamma(scrubber)
+  
+  scrubber.reject! { |line| line[i] == g[i] }
+
+  i += 1
+end
+
+
+pp oxygen.join.to_i(2) * scrubber.join.to_i(2)
+
