ec0fa4858fe86e0f613a67c39b3d02682edd91ca
Daniel Rødskog <danielmero@gmail.com>
2021-12-12 01:03:03 +0100

    Solve AOC2021 day 16 part 2 in Ruby

--- aoc2021/ruby/16.rb
+++ aoc2021/ruby/16.rb
@@ -4,21 +4,21 @@ def read_bits(input)
   input.chars.map { |c| c.to_i(16).to_s(2).rjust(4, '0') }.join
 end
 
-Packet = Struct.new(:version, :type, :data, :sub_packets, keyword_init: true)
+Packet = Struct.new(:version, :type, :value, :sub_packets, keyword_init: true)
 
 def parse(bits)
   version = bits.read(3).to_i(2)
   type = bits.read(3).to_i(2)
 
   if type == 4
-    data = ''
+    buffer = ''
     loop do
       prefix = bits.read(1)
-      data += bits.read(4)
+      buffer << bits.read(4)
       break if prefix == '0'
     end
 
-    return Packet.new(version:version, type:type, data:data)
+    return Packet.new(version:version, type:type, value:buffer.to_i(2))
   end
 
   length_type = bits.read(1).to_i(2)
@@ -42,23 +42,31 @@ def sum_version(packet)
   packet.version + sub_packets.sum { sum_version(_1) }
 end
 
-def part1(input)
-  bits = StringIO.new(read_bits input)
-  packets = parse bits
-  sum_version packets
+input = File.read('../input/16.txt').chomp
+bits = StringIO.new(read_bits input)
+packets = parse bits
+
+puts "Part 1: #{sum_version packets}"
+
+def calculate_value(packet)
+  case packet.type
+  when 0
+    packet.sub_packets.sum(&method(:calculate_value))
+  when 1
+    packet.sub_packets.map(&method(:calculate_value)).reduce(:*)
+  when 2
+    packet.sub_packets.map(&method(:calculate_value)).min
+  when 4
+    packet.value
+  when 3
+    packet.sub_packets.map(&method(:calculate_value)).max
+  when 5
+    packet.sub_packets.map(&method(:calculate_value)).reduce(:>) ? 1 : 0
+  when 6
+    packet.sub_packets.map(&method(:calculate_value)).reduce(:<) ? 1 : 0
+  when 7
+    packet.sub_packets.map(&method(:calculate_value)).reduce(:==) ? 1 : 0
+  end
 end
 
-input = File.read('../input/16.txt').chomp
-
-# pp part1(read_bits("D2FE28"))
-# pp part1(read_bits("38006F45291200"))
-# pp part1(read_bits("8A004A801A8002F478"))
-# pp part1(read_bits("620080001611562C8802118E34"))
-# pp part1(read_bits("EE00D40C823060"))
-# puts
-# pp part1(read_bits("8A004A801A8002F478"))
-# pp part1(read_bits("620080001611562C8802118E34"))
-# pp part1(read_bits("C0015000016115A2E0802F182340"))
-# pp part1(read_bits("A0016C880162017C3686B18A3D4780"))
-
-puts part1(input)
+puts "Part 2: #{calculate_value(packets)}"
