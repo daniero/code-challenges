f3344263d2f0c548db93aeaf43d44ef3a88da42c
Daniel Rødskog <danielmero@gmail.com>
2024-12-02 07:33:03 +0100

    Solve AOC2024 day 2 part 2 in Ruby

--- aoc2024/ruby/02.rb
+++ aoc2024/ruby/02.rb
@@ -3,7 +3,30 @@ input = File
   .map { |line| line.scan(/\d+/).map &:to_i }
 
 
+def normalize(a)
+  a == a.sort ? a : a.reverse
+end
+
+def is_safe(a)
+  a.each_cons(2).all? { |a,b| d = b - a; d >= 1 && d <= 3 }
+end
+
+def ok(a)
+  is_safe(normalize(a))
+end
+
+
 # Part 1
 
-pp input
-  .count { |r| (r == r.sort || r == r.sort.reverse) && r.each_cons(2).all? { |a,b| s = (a-b).abs; s >= 1 && s <= 3 } }
+p input.count { |a| ok(a) }
+
+
+# Part 2
+
+def dampen(a)
+  (0...a.length).map { |i| a[0...i] + a[(i+1)...a.length] }
+end
+
+p input.count { |a| ok(a) || dampen(a).any? { ok(_1) } }
+
+
