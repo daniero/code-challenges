63305f722c07f150f46b733260eb8596bd7f4aee
Daniel Rødskog <danielmero@gmail.com>
2024-12-03 17:11:24 +0100

    Simplify part 2

--- aoc2024/ruby/03.rb
+++ aoc2024/ruby/03.rb
@@ -1,21 +1,11 @@
 input = File.read('../input/03-sample.txt')
 
 # Part 1
-p input.scan(/mul\((\d+),(\d+)\)/).sum { |a,b| a.to_i * b.to_i }
+p input
+  .scan(/mul\((\d+),(\d+)\)/).sum { |a,b| a.to_i * b.to_i }
 
 
 # Part 2
-sum = 0
-enabled = true
-
-input.scan(/do(n't)?\(\)|mul\((\d+),(\d+)\)/) do
-  if $& == "do()"
-    enabled = true
-  elsif $& == "don't()"
-    enabled = false
-  elsif enabled
-    sum+= $2.to_i * $3.to_i
-  end
-end
-
-p sum
+p input
+  .gsub(/don't\(\).*?do\(\)/m,'---skip---')
+  .scan(/mul\((\d+),(\d+)\)/).sum { |a,b| a.to_i * b.to_i }
