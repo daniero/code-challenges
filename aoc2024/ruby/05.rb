6923d035a911359c626333c75bae423c28d0c553
Daniel Rødskog <danielmero@gmail.com>
2024-12-05 18:14:33 +0100

    Solve AOC2024 day 5 part 2 in Ruby

--- aoc2024/ruby/05.rb
+++ aoc2024/ruby/05.rb
@@ -7,7 +7,7 @@ input.scan(/(\d+)\|(\d+)/) { order[$1.to_i] << $2.to_i }
 
 updates = input[/(?<=\n\n).*/m].lines.map { _1.scan(/\d+/).map &:to_i }
 
-correct = updates.filter { |update|
+(correct, incorrect) = updates.partition { |update|
   update.map.with_index.all? { |page,i|
     update[0...i].none? { |before| order[page].include? before }
   }
@@ -15,3 +15,8 @@ correct = updates.filter { |update|
 
 # Part 1
 p correct.sum { _1[_1.length/2] }
+
+# Part 2
+p incorrect
+  .map { _1.sort { |a,b| order[a].include?(b) ? -1 : order[b].include?(a) ? 1 : 0 } }
+  .sum { _1[_1.length/2] }
