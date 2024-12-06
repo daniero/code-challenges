ab2c27f86fff781c3f4e4193706af1a13cceb26a
Daniel Rødskog <danielmero@gmail.com>
2024-12-06 19:44:46 +0100

    Solve AOC2025 day 6 part 2 in Ruby

--- aoc2024/ruby/06.rb
+++ aoc2024/ruby/06.rb
@@ -1,5 +1,3 @@
-require 'set'
-
 map = File
   .readlines('../input/06-sample.txt')
   .map(&:chomp)
@@ -7,26 +5,40 @@ map = File
 y = map.index { _1[?^] }
 x = map[y].index ?^
 
-visited = Set[]
+def run(map, x, y)
+  dirs = [ [-1,0], [0,1], [1,0], [0,-1] ]
+  dir = 0
 
-dirs = [ [-1,0], [0,1], [1,0], [0,-1] ]
-dir = 0
+  visited = {}
 
-loop do
-  visited << [x,y]
+  loop do
+   (visited[[x,y]] ||= []) << dir%4
 
-  vy, vx = dirs[dir%4]
-  ny, nx = y+vy, x+vx
+    vy, vx = dirs[dir%4]
+    ny, nx = y+vy, x+vx
 
-  break if ny < 0 || ny >= map.length || nx < 0 || nx >= map.first.length
-
-  next_step = map[ny][nx]
-
-  if next_step == '#'
-    dir += 1
-  else
-    y, x = ny, nx
+    if ny < 0 || ny >= map.length || nx < 0 || nx >= map.first.length
+      return visited, :out
+    elsif visited[[nx,ny]]&.include? dir%4
+      return visited, :loop
+    elsif map[ny][nx] == '#'
+      dir += 1
+    else
+      y, x = ny, nx
+    end
   end
 end
 
-p visited.size
+# Part 1
+visited,_ = run(map, x,y)
+p visited.values.reject(&:empty?).count
+
+# Part 2
+p (visited.keys - [x,y]).count { |vx,vy|
+  row = map[vy]
+  new_row = [row[0...vx] + '#' + row[vx+1..]]
+  new_map = map[0...vy] + new_row + map[vy+1..]
+
+  _,foo = run(new_map,x,y)
+  foo == :loop
+}
