3649c1a25a3eef2a872c097a4f89819e526a3595
Daniel Rødskog <danielmero@gmail.com>
2024-12-08 12:26:35 +0100

    Solve AOC2024 day 8 part 2 in Ruby

--- aoc2024/ruby/08.rb
+++ aoc2024/ruby/08.rb
@@ -10,14 +10,36 @@ input.each_with_index { |row,y|
   }
 }
 
+def in_bounds(input, x, y) =
+  y >= 0 &&
+  y < input.length &&
+  x >= 0 &&
+  x < input[y].length
+
 
 # Part 1
 
 p positions.flat_map { |char,positions|
-    positions.permutation(2).map { |(x1,y1),(x2,y2)|
-      [2*x1 - x2, 2*y1 - y2]
-    }.filter { |nx,ny|
-      ny >= 0 && ny < input.length && nx >= 0 && nx < input.first.length
+    positions
+      .permutation(2)
+      .map { |(x1,y1),(x2,y2)| [2*x1 - x2, 2*y1 - y2] }
+      .filter { |nx,ny| in_bounds(input, nx, ny) }
+  }
+  .uniq
+  .size
+
+
+# Part 2
+
+p positions.flat_map { |char,positions|
+    positions.permutation(2).flat_map { |(x1,y1),(x2,y2)|
+      dx, dy = x2-x1, y2-y1
+
+      0.step
+        .lazy
+        .map { |v| [x1 - dx*v, y1 - dy*v] }
+        .take_while { |nx,ny| in_bounds(input, nx, ny) }
+        .to_a
     }
   }
   .uniq
