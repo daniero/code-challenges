2b41ef16cb5d38d4e9fc6af9d603a0e6bb72a4ae
Daniel Rødskog <danielmero@gmail.com>
2024-12-10 18:11:13 +0100

    Cleanup

--- aoc2024/ruby/10.rb
+++ aoc2024/ruby/10.rb
@@ -1,14 +1,18 @@
-DIRS = [ [0,-1], [1,0], [0,1], [-1,0] ]
+DIRS = [[0,-1], [1,0], [0,1], [-1,0]]
 
-def in_bounds(grid, x, y) = y >= 0 && y < grid.length && x >= 0 && x < grid[y].length
+def in_bounds(grid, x, y) =
+  y >= 0 &&
+  y < grid.length &&
+  x >= 0 &&
+  x < grid[y].length
 
-def find_trails(grid, v, x, y)
+def find_trails(grid, x, y, v=0, steps=[[x,y]])
   return [] unless in_bounds(grid, x, y) && grid[y][x] == v
-  return [[x,y]] if v == 9
+  return [steps] if v == 9
 
-  DIRS
+  return DIRS
     .map { |dx,dy| [x+dx, y+dy] }
-    .flat_map { |nx, ny| find_trails(grid, v+1, nx, ny) }
+    .flat_map { |nx, ny| find_trails(grid, nx, ny, v+1, [*steps, [nx,ny]]) }
 end
 
 
@@ -16,8 +20,9 @@ input = File
   .readlines('../input/10-sample.txt', chomp: true)
   .map { _1.chars.map &:to_i }
 
-p input
-  .each_with_index
-  .sum { |row,y|
-    row.each_index.sum { |x| find_trails(input, 0, x, y).size }
-  }
+trails = input.flat_map.with_index { |row,y|
+  row.each_index.flat_map { |x| find_trails(input, x, y) }
+}
+
+p trails.group_by(&:first).sum { |_,trail| trail.map(&:last).uniq.size }
+p trails.size
