7bf9356ef19991bc7e9eda2560817acc10cf9b05
Daniel Rødskog <danielmero@gmail.com>
2024-12-18 21:00:02 +0100

    Solve AOC2024 day 18 part 2 in Ruby

--- aoc2024/ruby/18.rb
+++ aoc2024/ruby/18.rb
@@ -2,7 +2,9 @@
 require 'pqueue'
 require 'set'
 
-if false
+test = true
+
+if test
   SIZE = 7
   INPUT = '../input/18-sample.txt'
   WAIT = 12
@@ -14,16 +16,13 @@ end
 
 DIRS = [[1,0], [0,1], [-1,0], [0,-1]]
 
-
 def in_bounds(y, x) = y >= 0 && y < SIZE && x >= 0 && x < SIZE
-
 
 def make_grid(coords)
   grid = SIZE.times.map { Array.new(SIZE) }
   coords.each_with_index { |(x,y),i| grid[y][x] = i+1 }
   grid
 end
-
 
 def find_path(grid, start_time=0)
   start = [0,0]
@@ -34,7 +33,7 @@ def find_path(grid, start_time=0)
   visited = Set[]
 
   until queue.empty? do
-    pos,time = p queue.pop
+    pos,time = queue.pop
     next unless visited.add? pos
 
     if pos == target
@@ -59,6 +58,12 @@ input = File.readlines(INPUT).map { _1.split(',').map(&:to_i) }
 
 
 # Part 1
-
 grid = make_grid(input.take(WAIT))
 p find_path(grid, WAIT)
+
+
+# Part 2
+puts input.drop(WAIT).find.with_index { |(x,y),i|
+  grid[y][x] = i+WAIT
+  !find_path(grid, i+WAIT+1)
+}&.join(",")
