5d6657fb9aec18ec22d7b44cb28e27fb8dab7951
Daniel Rødskog <danielmero@gmail.com>
2024-12-20 16:22:18 +0100

    Solve AOC2024 day 19 part 2 in Ruby

--- aoc2024/ruby/19.rb
+++ aoc2024/ruby/19.rb
@@ -1,13 +1,28 @@
-def make(design, patterns)
-  design.empty? || patterns.any? { |pattern|
-    design.start_with?(pattern) && make(design[pattern.length ..], patterns)
-  }
-end
-
-
 input = File.readlines('../input/19-sample.txt', chomp: true)
 
 patterns = input.first.split(", ")
 designs = input.drop(2)
 
-p designs.count { make(_1, patterns) }
+
+# Part 1
+def part1(design, patterns)
+  design.empty? || patterns.any? { |pattern|
+    design.start_with?(pattern) && part1(design[pattern.length ..], patterns)
+  }
+end
+
+p designs.count { part1(_1, patterns) }
+
+
+# Part 2
+$cache = {""=>1}
+
+def part2(design, patterns)
+  $cache[design] ||= patterns.sum { |pattern|
+    next 0 unless design.start_with?(pattern)
+
+    part2(design[pattern.length ..], patterns)
+  }
+end
+
+p designs.sum { part2(_1, patterns) }
