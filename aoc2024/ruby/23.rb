88ad7e0527bb9942a94ef016f8c96a7782c75132
Daniel Rødskog <danielmero@gmail.com>
2024-12-23 17:08:33 +0100

    Solve AOC2024 day 23 part 2 in Ruby

--- aoc2024/ruby/23.rb
+++ aoc2024/ruby/23.rb
@@ -25,5 +25,21 @@ connections.each { |cmp,cons|
 
 p ticc
   .filter { |set| set.any? { _1.start_with? 't' } }
-  .tap { |sets| puts sets.sort.map { _1.join ',' } }
+ #.tap { |sets| puts sets.sort.map { _1.join ',' } }
   .size
+
+
+# Part 2
+subsets = [[]]
+
+connections.each { |cmp,cons|
+  new_subsets = []
+  subsets.each { |subset|
+    if subset.all? { cons.include? _1 }
+      new_subsets << subset + [cmp]
+    end
+  }
+  subsets += new_subsets
+}
+
+puts subsets.max_by(&:size).sort.join(',')
