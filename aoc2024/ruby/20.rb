4713983e9bcf146730ca2e6f5546c6bd1f8b9465
Daniel Rødskog <danielmero@gmail.com>
2025-01-03 01:49:18 +0100

    Optimize

--- aoc2024/ruby/20.rb
+++ aoc2024/ruby/20.rb
@@ -1,3 +1,5 @@
+SAVE = 100
+
 DIRS = [[0,-1], [1,0], [0,1], [-1,0]]
 
 def in_bounds(grid, y, x) = y >= 0 && y < grid.length && x >= 0 && x < grid[y].length
@@ -14,31 +16,26 @@ def find_path(maze)
       .find { |y,x| in_bounds(maze, y, x) && maze[y][x] != '#' && !path.include?([y,x]) }
 
     path[pos] = i
-    break if pos == target
+    return path if pos == target
   end
-
-  path
 end
 
-def find_shortcuts(path, maze)
-  path.flat_map { |(y,x),dist|
-    DIRS.filter_map { |dy,dx|
+def find_shortcuts(maze, path)
+  path.sum { |(y,x),dist|
+    DIRS.count { |dy,dx|
       ay,ax = y+dy, x+dx
       by,bx = y+dy*2, x+dx*2
       next unless in_bounds(maze, by, bx) && maze[ay][ax] == '#' && maze[by][bx] != '#'
 
       new_dist = path[[by,bx]]
-      next if new_dist < dist+2
-      path.keys.take(dist+1) + [[ay,ax]] + path.keys.drop(new_dist)
+      saved = new_dist - dist - 2
+      saved >= SAVE
     }
   }
 end
 
 
 input = File.readlines('../input/20.txt', chomp: true)
-start = input.filter_map.with_index { |row,y| row.index('S')&.then { |x| [y,x] } }
 
 path = find_path(input)
-shortcuts = find_shortcuts(path, input)
-
-p shortcuts.count { path.size - _1.size >= 100 }
+p find_shortcuts(input, path)
