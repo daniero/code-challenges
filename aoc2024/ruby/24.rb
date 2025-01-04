82c0d7db5bfd6c65fc2be52e439a89f99737e64c
Daniel Rødskog <danielmero@gmail.com>
2025-01-04 19:51:07 +0100

    Cleanup

--- aoc2024/ruby/24.rb
+++ aoc2024/ruby/24.rb
@@ -1,30 +1,36 @@
+def run(gates, values)
+  gates = gates.dup
+  values = values.dup
+
+  until gates.empty? do
+    gates
+      .select { |_,(in1, in2, _)| values[in1] && values[in2] }
+      .each { |out,(in1, in2, op)|
+        values[out] = op.call(values[in1], values[in2])
+        gates.delete(out)
+      }
+  end
+
+  values
+end
+
+
 input = File.read('../input/24.txt')
 
 values = {}
 input.scan(/(\w+): (\d)/) { values[$1] = $2.to_i }
 
+ops = {
+  "XOR" => lambda { _1 ^ _2 },
+  "OR" => lambda { _1 | _2 },
+  "AND" => lambda { _1 & _2 }
+}
+
 gates = {}
-input.scan(/(\w+) (XOR|OR|AND) (\w+) -> (\w+)/) { gates[$4] = [$1, $3, $2] }
+input.scan(/(\w+) (XOR|OR|AND) (\w+) -> (\w+)/) { gates[$4] = [$1, $3, ops[$2]] }
 
-def eval_out(a, b, op)
-  case op
-  when "XOR"; a^b
-  when "OR"; a|b
-  when "AND"; a&b
-  else; raise "invalid operator #{op}"
-  end
-end
-
-until gates.empty? do
-  gates
-    .select { |_,(in1, in2, _)| values[in1] && values[in2] }
-    .each { |out,(in1, in2, op)|
-      values[out] = eval_out(values[in1], values[in2], op)
-      gates.delete(out)
-    }
-end
-
-p values
+# Part 1
+p run(gates, values)
   .select { |key,_| key.start_with?('z') }
   .sort.reverse
   .map(&:last)
--- aoc2024/ruby/25.rb
+++ aoc2024/ruby/25.rb
@@ -1,11 +1,11 @@
 locks, keys = File
   .read('../input/25-sample.txt')
   .split("\n\n")
-  .map(&:split)
+  .map { _1.split.map(&:chars) }
   .partition { _1[0][0] == '#' }
 
 p locks.product(keys).count { |lock,key|
   lock.zip(key).all? { |lock_row, key_row|
-    lock_row.chars.zip(key_row.chars).all? { |l,k| l == '.' || k == '.' }
+    lock_row.zip(key_row).all? { _1.include? '.' }
   }
 }
