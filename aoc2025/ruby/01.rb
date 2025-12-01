51a4a95d1d6384d43949b2d7669c24cff699ad57
Daniel Rødskog <danielmero@gmail.com>
2025-12-01 18:15:38 +0100

    Solve AOC2025 day 1 part 2 in Ruby

--- aoc2025/ruby/01.rb
+++ aoc2025/ruby/01.rb
@@ -1,7 +1,9 @@
-moves = pp File
- #.readlines('../input-sample01.txt')
-  .readlines('../input-real01.txt')
+moves = File
+  .readlines('../input-sample01.txt')
+ #.readlines('../input-real01.txt')
   .map { it.tr('LR','-+').to_i }
+
+print "Part 1: "
 
 position = 50
 zeroes = 0
@@ -9,6 +11,23 @@ zeroes = 0
 moves.each do |move|
   position += move
   zeroes += 1 if position%100 == 0
+end
+
+p zeroes
+
+
+print "Part 2: "
+
+position = 50
+zeroes = 0
+
+moves.each do |move|
+  dir = move <=> 0
+
+  move.abs.times do
+    position += dir
+    zeroes += 1 if position%100 == 0
+  end
 end
 
 p zeroes
