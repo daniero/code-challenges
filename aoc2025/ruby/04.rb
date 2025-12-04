3c84be36636aa61ffac4d100f12f67067dbde355
Daniel Rødskog <danielmero@gmail.com>
2025-12-04 20:53:44 +0100

    Solve AOC 2025 day 4 part 2 in Ruby

--- aoc2025/ruby/04.rb
+++ aoc2025/ruby/04.rb
@@ -1,25 +1,42 @@
-#input = File.readlines('../input-real04.txt').map { it.chomp.chars }
-input = File.readlines('../input-sample04.txt').map { it.chomp.chars }
+#grid = File.readlines('../input-real04.txt').map { it.chomp.chars }
+grid = File.readlines('../input-sample04.txt').map { it.chomp.chars }
 
-puts q = input.each_with_index.map { |line,y|
-  line.each_with_index.map { |cell,x|
-    next cell if cell != '@'
+removed = 0
+total = 0
 
-    neighbours = 
-      [[y-1,x-1], [y-1,x], [y-1,x+1],
-       [y  ,x-1],          [y  ,x+1],
-       [y+1,x-1], [y+1,x], [y+1,x+1]]
+puts "Start"
+puts
+puts grid.map(&:join)
 
-    n = neighbours.filter { |ny,nx|
-      0 <= ny && ny < input.length &&
-      0 <= nx && nx < line.length
+loop do
+  grid = grid.each_with_index.map { |line,y|
+    line.each_with_index.map { |cell,x|
+      next cell if cell != '@'
+
+      neighbours = 
+        [[y-1,x-1], [y-1,x], [y-1,x+1],
+         [y  ,x-1],          [y  ,x+1],
+         [y+1,x-1], [y+1,x], [y+1,x+1]]
+
+      n = neighbours.filter { |ny,nx|
+        0 <= ny && ny < grid.length &&
+        0 <= nx && nx < line.length
+      }.count { |ny,nx|
+        grid[ny][nx] == '@'
+      }
+
+      n < 4 ? 'x' : '@'
     }
-    .count { |ny,nx|
-      input[ny][nx] == '@'
-    }
+  }
 
-    n < 4 ? 'x' : '@'
-  }.join
-}
+  removed = grid.map(&:join).join.count("x")
+  total+=removed
 
-p q.join.count("x")
+  puts "\nRemoved #{removed} - total #{total}\n\n"
+  break if removed == 0
+
+  puts grid.map(&:join)
+
+  grid.map! { |line| line.map { |char| char.sub("x",".") } }
+end
+
