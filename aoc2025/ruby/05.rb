963adf05040fe223cd44fdf105064d2f3c81a6b5
Daniel Rødskog <danielmero@gmail.com>
2025-12-05 19:54:44 +0100

    Solve AOC2025 day 5 part 2 in Ruby

--- aoc2025/ruby/05.rb
+++ aoc2025/ruby/05.rb
@@ -1,8 +1,29 @@
 a,b = File
-  .read('../input-real05.txt')
+  .read('input-05.txt')
   .split("\n\n")
 
-ranges = a.lines.map { it.scan(/\d+/).map(&:to_i).then { Range.new *it } }
+ranges = a.lines.map { Range.new *it.scan(/\d+/).map(&:to_i) }
 ids = b.lines.map(&:to_i)
 
-p ids.count { |id| ranges.any? { |range| range.cover? id } }
+
+print "Part 1: "
+puts ids.count { |id| ranges.any? { |range| range.cover? id } }
+
+
+print "Part 2: "
+
+without_overlap = ranges
+  .sort_by { [it.min, -it.size] }
+  .reduce([]) { |rs, r|
+    next [r] if rs.empty?
+    next rs if rs.last.cover? r
+    next rs if r.min < rs.last.min && r.max <= rs.last.max
+
+    start = [r.min, rs.last.max+1].max
+    next_range = Range.new(start, r.max)
+    next rs if rs.last.cover? next_range
+
+    [*rs, next_range]
+  }
+
+puts without_overlap.sum { it.size }
