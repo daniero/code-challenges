b64e8dfd6afad7a1187f9f1a24175e1e213374e5
Daniel Rødskog <danielmero@gmail.com>
2025-12-06 14:07:24 +0100

    Solve AOC2025 day 6 part 2 in Ruby

--- aoc2025/ruby/06.rb
+++ aoc2025/ruby/06.rb
@@ -1,7 +1,24 @@
-p File
-  .readlines('../input-real06')
-  .map { |line| line.split }
+input = File.readlines('../input-real06.txt')
+input = File.readlines('../input-sample06.txt')
+
+print "Part 1: "
+p input
+  .map(&:split)
   .transpose
   .sum { |*numbers, operator|
     numbers.map(&:to_i).reduce(&operator.to_sym)
   }
+
+
+print "Part 2: "
+p input
+  .map(&:chars)
+  .transpose
+  .map(&:join)
+  .reverse
+  .join
+  .strip
+  .gsub(/((\d+\s*)+)([+*])/) { "(#{$1.split.join($3)})" }
+  .split
+  .join(" + ")
+  .then { eval it }
