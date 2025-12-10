927cab38658db8ff04ca6328fa9d20b3a37fb2fb
Daniel Rødskog <danielmero@gmail.com>
2025-12-10 19:13:36 +0100

    Solve AOC2025 day 10 part 1 in Ruby

--- aoc2025/ruby/10.rb
+++ aoc2025/ruby/10.rb
@@ -1,19 +1,47 @@
 machines = File
-  .readlines('../input-sample10.txt')
+  #.readlines('../input-sample10.txt')
+  .readlines('../input-real10.txt')
   .map { |line|
     a,b,c = line.split(/(?<=\])\s|\s(?=\{)/)
 
-    lights = a.scan(/[.#]/).map { it == '#' }
-    buttons = b.split.map { it.scan(/\d+/).map(&:to_i) }
+    lights = a.scan(/[.#]/).map { it == '#' ? 1 : 0 }
+
+    buttons = b.split.map {
+      toggles = it.scan(/\d+/).map(&:to_i)
+      lights.size.times.map { |light|
+        toggles.include?(light) ? 1 : 0
+      }
+    }
+
     joltage = c.scan(/\d+/).map { it.to_i }
 
     [lights, buttons, joltage]
   }
 
-def part1(lights, buttons, joltage)
-  # todo
-  42
+
+def part1(target, buttons, _)
+  all_lights_off = target.map { 0 }
+  configurations = {all_lights_off => 0}
+
+  buttons.each { |button|
+    new_configurations = {}
+
+    configurations.each { |configuration,steps|
+      new_configuration = [configuration,button].transpose.map { it.reduce :^ } 
+
+      old = configurations[new_configuration]
+      if !old || old > steps+1
+        new_configurations[new_configuration] = steps+1
+      end
+    }
+
+    configurations.merge! new_configurations
+  }
+
+  configurations[target]
 end
 
-pp machines
-puts machines.sum { part1(*it) }
+
+puts machines.sum {
+  part1(*it)
+}
